db3b65d54b251a2443c8e864ea363804c0551bfb
0xNuggan <82726722+0xNuggan@users.noreply.github.com>
2022-09-29 10:07:15 +0200

    Chore: Export module documentation into readme (#22)
    * Move the module description to a new file
    
    * Update Module_Structure_Documentation.md
    
    * Update Module_Structure_Documentation.md

--- src/modules/base/Module.sol
+++ src/modules/base/Module.sol
@@ -23,52 +23,6 @@ import {IProposal} from "src/interfaces/IProposal.sol";
  * callbacks (via `call` or `delegatecall`) and a modifier to authenticate
  * callers via the module's proposal.
  *
- *
- * # Proposal Callbacks
- *
- * A module can trigger a callback from its proposal via the internal
- * `triggerProposalCallback(funcData, op)` function. The `op` argument specifies
- * whether the callback is executed via `call` or `delegatecall`, i.e. whether
- * the callback is executed in the module's or the proposal's context.
- *
- *
- * ## Callbacks executed in the Proposal's Context
- *
- * In order to easily access the proposals storage in proposal callback
- * functions executed via `delegatecall`, the contract inherits from the
- * auto-generated {ProposalStorage} contract to mirror the proposal's storage
- * layout. All variables inherited from the {ProposalStorage} are prefixed with
- * `__Proposal_`.
- *
- * In order to guarantee the callback is not executed in the module's context,
- * the `wantProposalContext` modifier MUST be used!
- *
- * Per convention, such `delegatecall`-callbacks SHOULD:
- * 1. Prefix the function name with `__Proposal_`
- * 2. Only access {ProposalStorage} variables.
- *
- *
- * ## Callbacks executed in the Module's Context
- *
- * Proposal callbacks executed in the module's context MUST be authenticated via
- * the `onlyProposal` modifier. They MUST NOT access `__Proposal_` variables.
- *
- * Per convention, the function name SHOULD be prefixed with `__Module_`.
- *
- *
- * # Initialization
- *
- * The contract provides a `__Module_init(proposal)` function for
- * initialization that MUST be called in order to correctly initialize the
- * storage.
- *
- *
- * # User Authentication
- *
- * Users are authenticated using the proposal's {IAuthenticator} instance.
- * This ensures that all access management is handled solely by the proposal.
- *
- *
  * # Versioning
  *
  * todo mp: Versioning
