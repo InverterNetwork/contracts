7b11c6123b66c3aa999a93a4e80a74f8436af6bd
pascal <pascal@merkleplant.xyz>
2022-09-29 16:47:45 +0200

    ModuleManagerMock: Inherit from ModuleManager, ie be real mock

--- test/utils/mocks/proposal/base/ModuleManagerMock.sol
+++ test/utils/mocks/proposal/base/ModuleManagerMock.sol
@@ -1,22 +1,14 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.0;
 
+// External Dependencies
+import {Initializable} from "@oz-up/proxy/utils/Initializable.sol";
+
 // Internal Dependencies
-import {Types} from "src/common/Types.sol";
+import {ModuleManager, Types} from "src/proposal/base/ModuleManager.sol";
 
-// Internal Interfaces
-import {IModuleManager} from "src/interfaces/IModuleManager.sol";
-
-contract ModuleManagerMock is IModuleManager {
-    function executeTxFromModule(
-        address to,
-        bytes memory data,
-        Types.Operation operation
-    ) external returns (bytes memory) {
-        return bytes("");
-    }
-
-    function isEnabledModule(address module) external returns (bool) {
-        return false;
+contract ModuleManagerMock is ModuleManager, Initializable {
+    function init(address[] calldata modules) external initializer {
+        __ModuleManager_init(modules);
     }
 }
