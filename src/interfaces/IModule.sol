4e933e41b414b92286bf6c44c87e393d6ffd531f
pascal <pascal@merkleplant.xyz>
2022-09-29 16:24:14 +0200

    Module: Cleaning and documentation

--- src/interfaces/IModule.sol
+++ src/interfaces/IModule.sol
@@ -2,6 +2,24 @@
 pragma solidity ^0.8.0;
 
 interface IModule {
+    //--------------------------------------------------------------------------
+    // Errors
+
+    /// @notice Function is only callable by authorized caller.
+    error Module__CallerNotAuthorized();
+
+    /// @notice Function is only callable by the proposal.
+    error Module__OnlyCallableByProposal();
+
+    /// @notice Given proposal address invalid.
+    error Module__InvalidProposalAddress();
+
+    /// @notice Function is not callable inside the module's context.
+    error Module__WantProposalContext();
+
+    //--------------------------------------------------------------------------
+    // Functions
+
     /// @notice Pauses the module.
     /// @dev Only callable by authorized addresses.
     function pause() external;
