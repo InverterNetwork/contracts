2adf42138716c70ab7d48088ab311c444d7385d1
SchneiderNejc <nejc.sch@gmail.com>
2022-10-05 11:47:39 +0200

    move Payment.sol into src/modules

--- src/modules/Payment.sol
+++ src/modules/Payment.sol
@@ -4,219 +4,123 @@ pragma solidity ^0.8.0;
 // Internal Dependencies
 import {Types} from "src/common/Types.sol";
 import {Module} from "src/modules/base/Module.sol";
-import {ERC20} from "@oz/token/ERC20/ERC20.sol";
-import {VestingWallet} from "@oz/finance/VestingWallet.sol";
 
 // Interfaces
 import {IERC20} from "@oz/token/ERC20/IERC20.sol";
 import {IProposal} from "src/interfaces/IProposal.sol";
 
-/*** @todo Nejc:
- -update formatting according to notion
- -implement vesting
- - `addPayment()` fetch token from address(proposal) to address(this).
- - replace require syntax with errors
-
- CHECKS:
- -mp: Define token in proposal, fetchable via `paymentToken()`.
-*/
-
-
-/**
- * @title Payment module
- *
- * @dev The payment module handles the money flow to the contributors
- * (e.g. how many tokens are sent to which contributor at what time).
- *
- * @author byterocket
- */
-
-
 contract Payment is Module {
+
     //--------------------------------------------------------------------------
     // Storage
 
-    ERC20 private token;
-    address private proposal;
-
-    struct PaymentStruct {
-        uint salary; // per epoch
+    struct Payment {
+        uint proposalId;
+        address contributor;
+        uint salary;            // per epoch
         uint epochsAmount;
-        bool enabled; // @audit rename to paused?
-    }
+        bool enabled;
+    };
 
-    // contributerAddress => Payment
-    mapping(address => PaymentStruct) public payments;
-
-    //address private token;
+    // proposalId => contributerAddress => Contributor
+    mapping(uint => mapping(address => Payment)) public payments;
 
     //--------------------------------------------------------------------------
     // Events
 
-    event PaymentAdded(address contributor, uint salary, uint epochsAmount);
-
-    event PaymentRemoved(address contributor, uint salary, uint epochsAmount);
-
-    event EnablePaymentToggled(address contributor, uint salary, bool enabled);
-
-    event PaymentClaimed(address contributor, uint availableToClaim);
-
-    //--------------------------------------------------------------------------
-    // Errors
-
-    // @todo Nejc: Declare here.
+    event addPayment(
+        uint proposalId,
+        address contributor,
+        uint salary,
+        uint epochsAmount
+    );
 
     //--------------------------------------------------------------------------
     // Modifiers
 
-    modifier validContributor(address contributor) {
+    modifier onlyOwner(uint proposalId) {
+        require(proposals[proposalId].owner == msg.sender, "not proposal owner");
+        _;
+    }
+
+    modifier onlyContributor(uint proposalId) {
+        require(payments[proposalId][msg.sender].salary > 0, "no active payments");
+        _;
+    }
+
+    // @dev make sure proposal exists
+    modifier proposalExists(uint proposalId) {
+        require(proposals[proposalId].initialFunding > 0, "proposal does not exist");
+        _;
+    }
+
+    modifier validContributorData(address contributorAddress, uint salary, uint epochsAmount) {
+        require(payments[proposalId][contributor].salary == 0, "payment already added");
         require(contributor != address(0), "invalid contributor address");
-        require(contributor != address(this), "invalid contributor address");
-        require(contributor != msg.sender, "invalid contributor address");
-        require(payments[contributor].salary == 0, "payment already added");
-        _;
-    }
-
-    modifier validSalary(uint salary) {
         require(salary > 0, "invalid salary");
-        _;
-    }
-
-    modifier validEpochsAmount(uint epochsAmount) {
-        require(epochsAmount > 0, "invalid epochs amount");
-        _;
-    }
-
-    modifier hasActivePayments() {
-        require(payments[msg.sender].salary > 0, "no active payments");
+        require(epochsAmount > 0, "invalid epochsAmount");
         _;
     }
 
     //--------------------------------------------------------------------------
-    // External Functions
+    // Functions
 
-    /// @notice Initialize module, save token and proposal address.
-    /// @param proposalInterface Interface of proposal.
-    /// @param data encoded token and proposal address.
-    function initialize(IProposal proposalInterface, bytes memory data)
-        external
-        initializer
-    {
-        __Module_init(proposalInterface);
+    function initialize(IProposal proposal, bytes memory) external {
+        __Module_init(proposal);
 
-        (address _token, address _proposal) =
-            abi.decode(data, (address, address));
-
-        require(validAddress(_token), "invalid token address");
-        require(validAddress(_proposal), "invalid proposal address");
-
-        token = ERC20(_token);
-        proposal = _proposal;
+        // @note Decode params like:
+        // (uint a) = abi.decode(data, (uint));
     }
 
-    /// @notice Claims any accrued funds which a contributor has earnt.
-    function claim() external hasActivePayments() {
-        // @todo Nejc: Implement.
-        uint availableToClaim;
-
-        emit PaymentClaimed(msg.sender, availableToClaim);
+    // @notice Claims any accrued funds which a contributor has earnt
+    function claim(uint proposalId) external onlyContributor(proposalId) {
+        // TODO implement
     }
 
-    /// @notice Removes/stops a payment of a contributor.
-    /// @param contributor Contributor's address.
-    function removePayment(address contributor)
-        external
-        onlyAuthorized() // only proposal owner
-    {
-        if (payments[contributor].salary != 0) {
-            uint _salary = payments[contributor].salary;
-            uint _epochsAmount = payments[contributor].epochsAmount;
-
-            delete payments[contributor];
-
-            emit PaymentRemoved(contributor, _salary, _epochsAmount);
-        }
+    // @notice Removes/stops a payment of a contributor
+    function removePayment(uint proposalId) external onlyOwner(proposalId) {
+        require(payments[proposalId][contributor].salary > 0, "non existing payment");
+        payments[proposalId][contributor].enabled = false;
     }
 
-    /// @notice Enable/Disable a payment of a contributor.
-    /// @param contributor Contributor's address.
-    function toggleEnablePayment(address contributor)
-        external
-        onlyAuthorized() // only proposal owner
-    {
-        payments[contributor].enabled = !payments[contributor].enabled;
-
-        emit EnablePaymentToggled(
-            contributor,
-            payments[contributor].salary,
-            payments[contributor].enabled
-        );
+    // @notice Pauses a payment of a contributor
+    function pausePayment(uint proposalId) external onlyOwner(proposalId) {
+        require(payments[proposalId][contributor].salary > 0, "non existing payment");
+        payments[proposalId][contributor].enabled = false;
     }
 
-    /// Note we may want a method that returns all contributor addresses.
-    /// @notice Returns the existing payments of the contributors.
-    /// @param contributor Contributor's address.
-    /// @return salary Salary contributor will receive per epoch.
-    /// @return epochsAmount Amount of epochs to receive the salary for.
-    function listPayments(address contributor)
+    /// @notice Returns the existing payments of the contributors
+    function listPayments(uint proposalId, address contributorAddress)
         external
         view
-        returns (uint, uint)
+        proposalExists(proposalId)
+        returns(uint, uint)
     {
-        return (
-            payments[contributor].salary,
-            payments[contributor].epochsAmount
-        );
+        return (payments[proposalId][contributorAddress].salary, payments[proposalId][contributorAddress].epochsAmount);
     }
 
-    /// @notice Adds a new payment containing the details of the monetary flow
-    ///         depending on the module.
-    /// @param contributor Contributor's address.
-    /// @param salary Salary contributor will receive per epoch.
-    /// @param epochsAmount Amount of epochs to receive the salary for.
-    function addPayment(
-        address contributor,
-        uint salary,
-        uint epochsAmount
-    )
+    /// @notice Adds a new payment containing the details of the monetary flow depending on the module
+    function addPayment(uint proposalId, address contributorAddress, uint salary, uint epochsAmount)
         external
-        onlyAuthorized() // only proposal owner
-        validContributor(contributor)
-        validSalary(salary)
-        validEpochsAmount(epochsAmount)
+        proposalExists(proposalId)
+        validContributorData(contributorAddress, salary, epochsAmount)
     {
-        // Somewhere else:
-        //(A sends X tokens to proposal => token.balanceOf(proposal) == X)
-        // Payment:
-        // function addPayment {
-        //   token.transferFrom(proposal, address(this), amount);
-        // }
+        // @todo verify there's enough tokens in proposal for the payout.
 
-        // @todo Nejc: Verify there's enough tokens in proposal for the payment.
-
-        // @todo Nejc: Ensure token address is the same as defined in proposal.
-
-        // Note Before adding payment make sure contributor is wListed.
-
-        // @dev add struct data to mapping
-        payments[contributor] = PaymentStruct(
+        // add struct data to mapping
+        payments[proposalId][contributorAddress] = Payment(
+            proposalId,
+            contributorAddress,
             salary,
             epochsAmount,
             true
         );
 
-        emit PaymentAdded(contributor, salary, epochsAmount);
+        emit addPayment(proposalId, contributorAddress, salary, epochsAmount);
     }
 
-    //--------------------------------------------------------------------------
-    // Internal Functions
 
-    /// @notice validate address input.
-    /// @param addr Address to validate.
-    /// @return True if address is valid.
-    function validAddress(address addr) internal view returns(bool){
-        if(addr != address(0) || addr != msg.sender || addr != address(this))
-            return false;
-        return true;
-    }
+
+
+
 }
