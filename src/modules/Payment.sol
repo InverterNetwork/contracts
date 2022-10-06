c478c9a290ed50f5c4f73d434a84af439e6d9250
SchneiderNejc <nejc.sch@gmail.com>
2022-10-06 23:37:45 +0200

    Payment.sol: use forge fmt

--- src/modules/Payment.sol
+++ src/modules/Payment.sol
@@ -11,17 +11,17 @@ import {VestingWallet} from "@oz/finance/VestingWallet.sol";
 import {IERC20} from "@oz/token/ERC20/IERC20.sol";
 import {IProposal} from "src/interfaces/IProposal.sol";
 
-/*** TODO:
- -update formatting according to notion
- -implement vesting
- - `addPayment()` fetch token from address(proposal) to address(this).
- - Refactor modifiers to only have single arguments, e.g. `validSalary`, `validEpochs`.
- - replace require syntax with errors
-
- CHECKS:
- -mp: Define token in proposal, fetchable via `paymentToken()`.
-*/
-
+/**
+ * TODO:
+ * -update formatting according to notion
+ * -implement vesting
+ * - `addPayment()` fetch token from address(proposal) to address(this).
+ * - Refactor modifiers to only have single arguments, e.g. `validSalary`, `validEpochs`.
+ * - replace require syntax with errors
+ *
+ * CHECKS:
+ * -mp: Define token in proposal, fetchable via `paymentToken()`.
+ */
 
 contract Payment is Module {
     //--------------------------------------------------------------------------
@@ -67,10 +67,9 @@ contract Payment is Module {
     }
 
     modifier validSalary(address contributor, uint salary) {
-      require(payments[contributor].salary == 0,
-          "payment already added");
-      require(salary > 0, "invalid salary");
-      _;
+        require(payments[contributor].salary == 0, "payment already added");
+        require(salary > 0, "invalid salary");
+        _;
     }
 
     modifier validEpochsAmount(uint epochsAmount) {
@@ -79,9 +78,7 @@ contract Payment is Module {
     }
 
     modifier hasActivePayments() {
-        require(
-            payments[msg.sender].salary > 0, "no active payments"
-        );
+        require(payments[msg.sender].salary > 0, "no active payments");
         _;
     }
 
@@ -89,7 +86,8 @@ contract Payment is Module {
     // Functions
 
     function initialize(IProposal proposal, bytes memory data)
-        external initializer
+        external
+        initializer
     {
         __Module_init(proposal);
 
@@ -100,7 +98,7 @@ contract Payment is Module {
     }
 
     // @notice Claims any accrued funds which a contributor has earnt
-    function claim() external hasActivePayments() {
+    function claim() external hasActivePayments {
         // TODO implement
         uint availableToClaim;
 
@@ -110,7 +108,7 @@ contract Payment is Module {
     // @notice Removes/stops a payment of a contributor
     function removePayment(address contributor)
         external
-        onlyAuthorized() // only proposal owner
+        onlyAuthorized // only proposal owner
     {
         if (payments[contributor].salary != 0) {
             uint _salary = payments[contributor].salary;
@@ -126,7 +124,7 @@ contract Payment is Module {
     // @param contributor Contributor's address
     function toggleEnablePayment(address contributor)
         external
-        onlyAuthorized() // only proposal owner
+        onlyAuthorized // only proposal owner
     {
         payments[contributor].enabled = !payments[contributor].enabled;
 
@@ -134,7 +132,7 @@ contract Payment is Module {
             contributor,
             payments[contributor].salary,
             payments[contributor].enabled
-        );
+            );
     }
 
     /// NOTE we may want a method that returns all the contributor addresses
@@ -144,21 +142,14 @@ contract Payment is Module {
         view
         returns (uint, uint)
     {
-        return (
-            payments[contributor].salary,
-            payments[contributor].epochsAmount
-        );
+        return (payments[contributor].salary, payments[contributor].epochsAmount);
     }
 
     /// @notice Adds a new payment containing the details of the monetary flow
     ///         depending on the module
-    function addPayment(
-        address contributor,
-        uint salary,
-        uint epochsAmount
-    )
+    function addPayment(address contributor, uint salary, uint epochsAmount)
         external
-        onlyAuthorized() // only proposal owner
+        onlyAuthorized // only proposal owner
         validContributor(contributor)
         validSalary(contributor, salary)
         validEpochsAmount(epochsAmount)
@@ -174,11 +165,7 @@ contract Payment is Module {
         // @todo make sure token address is the same as defined in proposal
 
         // add struct data to mapping
-        payments[contributor] = Payment(
-            salary,
-            epochsAmount,
-            true
-        );
+        payments[contributor] = Payment(salary, epochsAmount, true);
 
         emit PaymentAdded(contributor, salary, epochsAmount);
     }
