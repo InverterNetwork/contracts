3731c718130b70bebf8296a8da141c254c87aa8c
SchneiderNejc <nejc.sch@gmail.com>
2022-10-12 16:26:01 +0200

    Payment.sol: contract overhaul - merge OZ VestingWallet into base contract

--- src/modules/Payment.sol
+++ src/modules/Payment.sol
@@ -5,19 +5,14 @@ pragma solidity ^0.8.0;
 import {Types} from "src/common/Types.sol";
 import {Module} from "src/modules/base/Module.sol";
 import {ERC20} from "@oz/token/ERC20/ERC20.sol";
-import {VestingWallet} from "@oz/finance/VestingWallet.sol";
 
 // Interfaces
 import {IERC20} from "@oz/token/ERC20/IERC20.sol";
 import {IProposal} from "src/interfaces/IProposal.sol";
 
-/*** @todo Nejc:
- -implement vesting
- - `addPayment()` fetch token from address(proposal) to address(this).
- - replace require syntax with errors
 
- CHECKS:
- -mp: Define token in proposal, fetchable via `paymentToken()`.
+/*** @todo Nejc:
+ - replace require syntax with errors
 */
 
 
@@ -36,38 +31,38 @@ contract Payment is Module {
     // Storage
 
     IERC20 private token;             // invariant
-    VestingWallet private vesting;    // invariant
-
     address private proposal;
 
-    struct PaymentStruct {
-        address vesting;
-        uint salary; // per epoch
-        uint epochsAmount;
-        bool enabled; // @audit rename to paused?
+    struct VestingWallet {
+        uint _salary;
+        uint _released;
+        uint64 _start;
+        uint64 _duration;
+        bool enabled;
     }
 
     // contributor => Payment
-    mapping(address => PaymentStruct) public payments;
-
-    // contributor => vesting
-    mapping(address => address) public vestings;
+    mapping(address => VestingWallet) private vestings;
 
     //--------------------------------------------------------------------------
     // Events
 
-      event PaymentAdded(
+    event ERC20Released(address indexed token, uint256 amount);
+
+    event PaymentAdded(
         address contributor,
         uint salary,
         uint64 start,
         uint64 end
     );
 
-    event PaymentRemoved(address contributor, uint salary, uint epochsAmount);
+    // event PaymentRemoved(address contributor, uint salary, uint epochsAmount);
+    //
+    // event EnablePaymentToggled(address contributor, uint salary, bool enabled);
+    //
+    // event PaymentClaimed(address contributor, uint availableToClaim);
 
-    event EnablePaymentToggled(address contributor, uint salary, bool enabled);
 
-    event PaymentClaimed(address contributor, uint availableToClaim);
 
     //--------------------------------------------------------------------------
     // Errors
@@ -81,7 +76,7 @@ contract Payment is Module {
         require(contributor != address(0), "invalid contributor address");
         require(contributor != address(this), "invalid contributor address");
         require(contributor != msg.sender, "invalid contributor address");
-        require(payments[contributor].salary == 0, "payment already added");
+        require(vestings[contributor]._salary == 0, "payment already added");
         _;
     }
 
@@ -96,13 +91,57 @@ contract Payment is Module {
     }
 
     modifier hasActivePayments() {
-        require(payments[msg.sender].salary > 0, "no active payments");
+        require(vestings[msg.sender]._salary > 0, "no active payments");
         _;
     }
 
     //--------------------------------------------------------------------------
-    // External Functions
+    // External View Functions
 
+    function start(address contributor) public view virtual returns(uint256) {
+        return vestings[contributor]._start;
+    }
+
+    function duration(address contributor) public view virtual returns(uint256) {
+        return vestings[contributor]._duration;
+    }
+
+    function released(address contributor) public view virtual returns(uint256) {
+        return vestings[contributor]._released;
+    }
+
+    function vestedAmount(uint64 timestamp) public view virtual returns (uint256) {
+        return _vestingSchedule(token.balanceOf(
+            address(this)) + released(msg.sender), timestamp);
+    }
+
+    function releasable() public view virtual returns (uint) {
+        return vestedAmount(uint64(block.timestamp)) - released(msg.sender);
+    }
+
+    function release() public virtual {
+        uint256 amount = releasable();
+        vestings[msg.sender]._released += amount;
+        emit ERC20Released(address(token), amount);
+        // @todo Nejc: check transfer return value / use safeTransfer
+        token.transfer(msg.sender, amount);
+    }
+
+    function _vestingSchedule(uint256 totalAllocation, uint64 timestamp)
+        internal
+        view
+        virtual
+        returns (uint256)
+    {
+        if (timestamp < start(msg.sender)) {
+            return 0;
+        } else if (timestamp > start(msg.sender) + duration(msg.sender)) {
+            return totalAllocation;
+        } else {
+            return (totalAllocation * (timestamp - start(msg.sender))) /
+                duration(msg.sender);
+        }
+    }
 
     /// @notice Initialize module, save token and proposal address.
     /// @param proposalInterface Interface of proposal.
@@ -146,94 +185,74 @@ contract Payment is Module {
         //validEpochsAmount(epochsAmount)
         // @todo Nejc: add modifiers for input validation
     {
-
         // INPUTS VALIDATION
         // require(start + duration > start, "duration overflow");
         // require(start > block.timestamp, "should start in future");
         // require(duration > 0, "duration cant be 0");
         //require(vestings[contributor] == address(0), "already has a vesting");
 
-        vesting = new VestingWallet(contributor, start, duration);
-
         // @todo Nejc: Verify there's enough tokens in proposal for the payment.
-        // @todo Nejc transferFrom proposal instead this.
+        // @todo Nejc transferFrom proposal to payment module.
         // @todo Nejc: before adding payment make sure contributor is wListed.
 
-        vesting = new VestingWallet(contributor, start, duration);
+        vestings[msg.sender] = VestingWallet(
+            salary, 0, start, duration, true
+        );
 
-        token.transfer(address(vesting), salary);
-
-        vestings[contributor] = address(vesting);
-
-        // @dev add struct data to mapping
-        // payments[contributor] = PaymentStruct(
-        //     salary,
-        //     epochsAmount,
-        //     true
-        // );
-
-        emit PaymentAdded(contributor, salary, start, start + duration);
+        emit PaymentAdded(contributor, salary, start, duration);
     }
 
     // @notice Returns address of vesting contract per contributor.
-    function getVesting(address contributor) external view returns(address) {
-        return vestings[contributor];
-    }
-
-    /// @notice Claims any accrued funds which a contributor has earnt.
-    function claim() external hasActivePayments() {
-        // @todo Nejc: Implement.
-        uint availableToClaim;
-
-        emit PaymentClaimed(msg.sender, availableToClaim);
-    }
+    // function getVesting(address contributor) external view returns(address) {
+    //     return vestings[contributor];
+    // }
 
     /// @notice Removes/stops a payment of a contributor.
     /// @param contributor Contributor's address.
-    function removePayment(address contributor)
-        external
-        onlyAuthorized() // only proposal owner
-    {
-        if (payments[contributor].salary != 0) {
-            uint _salary = payments[contributor].salary;
-            uint _epochsAmount = payments[contributor].epochsAmount;
-
-            delete payments[contributor];
-
-            emit PaymentRemoved(contributor, _salary, _epochsAmount);
-        }
-    }
-
-    /// @notice Enable/Disable a payment of a contributor.
-    /// @param contributor Contributor's address.
-    function toggleEnablePayment(address contributor)
-        external
-        onlyAuthorized() // only proposal owner
-    {
-        payments[contributor].enabled = !payments[contributor].enabled;
-
-        emit EnablePaymentToggled(
-            contributor,
-            payments[contributor].salary,
-            payments[contributor].enabled
-        );
-    }
-
-    /// Note we may want a method that returns all contributor addresses.
-    /// @notice Returns the existing payments of the contributors.
-    /// @param contributor Contributor's address.
-    /// @return salary Salary contributor will receive per epoch.
-    /// @return epochsAmount Amount of epochs to receive the salary for.
-    function listPayments(address contributor)
-        external
-        view
-        returns (uint, uint)
-    {
-        return (
-            payments[contributor].salary,
-            payments[contributor].epochsAmount
-        );
-    }
+    // function removePayment(address contributor)
+    //     external
+    //     onlyAuthorized() // only proposal owner
+    // {
+    //     if (payments[contributor].salary != 0) {
+    //         uint _salary = payments[contributor].salary;
+    //         uint _epochsAmount = payments[contributor].epochsAmount;
+    //
+    //         delete payments[contributor];
+    //
+    //         emit PaymentRemoved(contributor, _salary, _epochsAmount);
+    //     }
+    // }
+    //
+    // /// @notice Enable/Disable a payment of a contributor.
+    // /// @param contributor Contributor's address.
+    // function toggleEnablePayment(address contributor)
+    //     external
+    //     onlyAuthorized() // only proposal owner
+    // {
+    //     payments[contributor].enabled = !payments[contributor].enabled;
+    //
+    //     emit EnablePaymentToggled(
+    //         contributor,
+    //         payments[contributor].salary,
+    //         payments[contributor].enabled
+    //     );
+    // }
+    //
+    // /// Note we may want a method that returns all contributor addresses.
+    // /// @notice Returns the existing payments of the contributors.
+    // /// @param contributor Contributor's address.
+    // /// @return salary Salary contributor will receive per epoch.
+    // /// @return epochsAmount Amount of epochs to receive the salary for.
+    // function listPayments(address contributor)
+    //     external
+    //     view
+    //     returns (uint, uint)
+    // {
+    //     return (
+    //         payments[contributor].salary,
+    //         payments[contributor].epochsAmount
+    //     );
+    // }
 
     //--------------------------------------------------------------------------
     // Internal Functions
