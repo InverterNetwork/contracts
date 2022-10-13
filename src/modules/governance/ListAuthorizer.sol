c3660fa74ef08135651a17c516bd17791640ac50
0xNuggan <82726722+0xNuggan@users.noreply.github.com>
2022-10-13 14:33:54 +0200

    Modules: Adds ListAuthorizer and ContributorManager
    Also a draft version of a SingleVoteGovernance contract.
    
    Closes #17 
    
    * Move Governance.sol and create ContributorManager.sol
    
    First drafts of Governance module and contributor manager
    
    * Update ContributorManager.sol
    
    * continue basic setup of contributorManager.sol
    
    * Update ContributorManager.sol
    
    * refactor and comment ContributorManagement.sol
    
    * Update Governance.sol
    
    * Update Governance.sol
    
    * work on Governanve Module
    
    Finish the functions, refactor to correct call structure and add comments
    
    * formatting
    
    * split governance + first batch of review fixes
    
    * Implement inheritance in Governance + other dev
    
    * formatting
    
    * Modifications to conttribmanager + Contribmanager tests
    
    * Add ListAuthorizer Tests + small governance access fix
    
    * Comments from code review

---
+++ src/modules/governance/ListAuthorizer.sol
@@ -0,0 +1,170 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.0;
+
+// Internal Dependencies
+import {Types} from "src/common/Types.sol";
+import {ContextUpgradeable} from "@oz-up/utils/ContextUpgradeable.sol";
+import {Module} from "src/modules/base/Module.sol";
+
+// Interfaces
+import {IProposal} from "src/interfaces/IProposal.sol";
+import {IAuthorizer} from "src/interfaces/IAuthorizer.sol";
+
+/**
+ * @title A simple List-based Authorizer
+ *
+ * @dev
+ * This Module handles the authorization of several of the smart contract
+ * functions
+ *
+ * It keeps a list authorized addresses and implements the IAuthorizers
+ * abstract function "isAuthorized", necessary for the "onlyAuthorized" modifier
+ * in the base module contract.
+ *
+ * The authorized addresses can be anything, ranging from an EOA to a Gnosis
+ * multisignature, keeping the module agnostic to the specific governance
+ * structure employed.
+ */
+
+contract ListAuthorizer is IAuthorizer, Module {
+    //--------------------------------------------------------------------------
+    // Errors
+    error Module__AddressAlreadyAuthorized();
+    error Module__AddressAlreadyNotAuthorized();
+
+    //--------------------------------------------------------------------------
+    // Events
+
+    event AddedAuthorizedAddress(address added);
+
+    event RemovedAuthorizedAddress(address removed);
+
+    //--------------------------------------------------------------------------
+    // Storage
+
+    /// @dev Mapping of authorized addresses
+    mapping(address => bool) private authorized;
+    uint private amountAuthorized;
+
+    //--------------------------------------------------------------------------
+    // Initialization
+
+    function initialize(IProposal proposal, Metadata memory metadata)
+        external
+        initializer
+    {
+        __Module_init(proposal, metadata);
+
+        //authorize the calling address
+        authorized[_msgSender()] = true;
+        amountAuthorized++;
+
+        emit AddedAuthorizedAddress(_msgSender());
+    }
+
+    //--------------------------------------------------------------------------
+    // Public Functions
+
+    /// @notice Returns whether an address is authorized to facilitate
+    ///         the current transaction.
+    /// @param  _who  The address on which to perform the check.
+    function isAuthorized(address _who) public view override returns (bool) {
+        return authorized[_who];
+    }
+
+    /// @notice Returns the number of authorized addresses
+    function getAmountAuthorized() public view returns (uint) {
+        return amountAuthorized;
+    }
+
+    /// @notice Adds a new address to the list of authorized addresses.
+    /// @param _who The address to add to the list of authorized addresses.
+    function __ListAuthorizer_addToAuthorized(address _who)
+        public
+        virtual
+        onlyProposal
+    {
+        if (!isAuthorized(_who)) {
+            authorized[_who] = true;
+            amountAuthorized++;
+
+            emit AddedAuthorizedAddress(_who);
+        }
+    }
+
+    /// @notice Adds a new address to the list of authorized addresses.
+    /// @dev Relay Function that routes the function call via the proposal
+    /// @param _who The address to add to the list of authorized addresses.
+    function addToAuthorized(address _who) external onlyAuthorized {
+        _triggerProposalCallback(
+            abi.encodeWithSignature(
+                "__ListAuthorizer_addToAuthorized(address)", _who
+            ),
+            Types.Operation.Call
+        );
+    }
+
+    /// @notice Removes an address from the list of authorized addresses.
+    /// @param _who Address to remove authorization from
+    function __ListAuthorizer_removeFromAuthorized(address _who)
+        public
+        virtual
+        onlyProposal
+    {
+        //@question TODO Do we want to allow an empty authorizer list?
+        //          My first impulse would be no...
+
+        if (isAuthorized(_who)) {
+            authorized[_who] = false;
+            amountAuthorized--;
+
+            emit RemovedAuthorizedAddress(_who);
+        }
+    }
+
+    /// @notice Removes an address from the list of authorized addresses.
+    /// @dev Relay Function that routes the function call via the proposal
+    /// @param _who Address to remove authorization from
+    function removeFromAuthorized(address _who) external onlyAuthorized {
+        _triggerProposalCallback(
+            abi.encodeWithSignature(
+                "__ListAuthorizer_removeFromAuthorized(address)", _who
+            ),
+            Types.Operation.Call
+        );
+    }
+
+    /// @notice Transfers authorization from the calling address to a new one.
+    /// @param _from The address to transfer the authorization from
+    /// @param _to The address to transfer the authorization to
+    function __ListAuthorizer_transferAuthorization(address _from, address _to)
+        public
+        virtual
+        onlyProposal
+    {
+        //I think in this case we actually DO want to revert if the receiving address is already authorized to avoid confusion. The opposite could lead to think that somebedy has "double vote" or something like that. It also saves us implementing quorum management logic downstream.
+        if (authorized[_to]) {
+            revert Module__AddressAlreadyAuthorized();
+        }
+
+        authorized[_to] = true;
+        authorized[_from] = false;
+
+        emit AddedAuthorizedAddress(_to);
+        emit RemovedAuthorizedAddress(_from);
+    }
+
+    /// @notice Transfers authorization from the calling address to a new one.
+    /// @dev Relay Function that routes the function call via the proposal
+    /// @param _who The address to transfer the authorization to
+    function transferAuthorization(address _who) external onlyAuthorized {
+        _triggerProposalCallback(
+            abi.encodeWithSignature(
+                "__ListAuthorizer_transferAuthorization(address)",
+                _msgSender(),
+                _who
+            ),
+            Types.Operation.Call
+        );
+    }
+}
---
+++ test/modules/ListAuthorizer.t.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+
+// Internal Dependencies
+import {Proposal} from "src/proposal/Proposal.sol";
+import {ContributorManager} from "src/modules/governance/ContributorManager.sol";
+import {ListAuthorizer} from "src/modules/governance/ListAuthorizer.sol";
+
+// Interfaces
+import {IAuthorizer} from "src/interfaces/IAuthorizer.sol";
+import {IProposal} from "src/interfaces/IProposal.sol";
+import {IModule} from "src/interfaces/IModule.sol";
+
+// Mocks
+import {ProposalMock} from "test/utils/mocks/proposal/ProposalMock.sol";
+import {AuthorizerMock} from "test/utils/mocks/AuthorizerMock.sol";
+import {ERC20Mock} from "test/utils/mocks/ERC20Mock.sol";
+
+contract ListAuthorizerTest is Test {
+    // Mocks
+    ListAuthorizer authorizer;
+    ProposalMock proposal;
+    address ALBA = address(0xa1ba);
+    address BOB = address(0xb0b);
+
+    function setUp() public {
+        authorizer = new ListAuthorizer();
+
+        proposal = new ProposalMock(authorizer);
+
+        // Initialize the Authorizer
+        IModule.Metadata memory data =
+            IModule.Metadata(1, "https://www.github.com");
+
+        authorizer.initialize(IProposal(proposal), data);
+
+        //authorize one address and deauthorize the deployer.
+        vm.prank(address(proposal));
+        authorizer.__ListAuthorizer_addToAuthorized(ALBA);
+
+        vm.prank(address(proposal));
+        authorizer.__ListAuthorizer_removeFromAuthorized(address(this));
+
+        assertEq(authorizer.isAuthorized(ALBA), true);
+        assertEq(authorizer.isAuthorized(address(this)), false);
+        assertEq(authorizer.getAmountAuthorized(), 1);
+    }
+
+    function testAddAuthorized() public {
+        uint amountAuth = authorizer.getAmountAuthorized();
+
+        vm.prank(address(proposal));
+        authorizer.__ListAuthorizer_addToAuthorized(BOB);
+
+        assertEq(authorizer.isAuthorized(BOB), true);
+        assertEq(authorizer.getAmountAuthorized(), (amountAuth + 1));
+    }
+
+    function testRemoveAuthorized() public {
+        //this test leaves an empty authorizer list. If we choose to disallow that it will need to be cahnged.
+        uint amountAuth = authorizer.getAmountAuthorized();
+
+        vm.prank(address(proposal));
+        authorizer.__ListAuthorizer_removeFromAuthorized(ALBA);
+
+        assertEq(authorizer.isAuthorized(ALBA), false);
+        assertEq(authorizer.getAmountAuthorized(), (amountAuth - 1));
+    }
+
+    function testTransferAuthorization() public {
+        uint amountAuth = authorizer.getAmountAuthorized();
+
+        vm.prank(address(proposal));
+        authorizer.__ListAuthorizer_transferAuthorization(ALBA, BOB);
+
+        assertEq(authorizer.isAuthorized(ALBA), false);
+        assertEq(authorizer.isAuthorized(BOB), true);
+        assertEq(authorizer.getAmountAuthorized(), (amountAuth));
+    }
+
+    function testAccessControl() public {
+        uint amountAuth = authorizer.getAmountAuthorized();
+
+        //test if a non authorized address fails authorization
+        address SIFU = address(0x51f00);
+        assertEq(authorizer.isAuthorized(SIFU), false);
+
+        //add authorized address/remove it and test authorization
+
+        vm.startPrank(address(proposal));
+        authorizer.__ListAuthorizer_addToAuthorized(BOB);
+        authorizer.__ListAuthorizer_removeFromAuthorized(BOB);
+        vm.stopPrank();
+
+        assertEq(authorizer.isAuthorized(BOB), false);
+        assertEq(authorizer.getAmountAuthorized(), (amountAuth));
+    }
+}
