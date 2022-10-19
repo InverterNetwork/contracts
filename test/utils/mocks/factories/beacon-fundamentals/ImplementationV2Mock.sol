afb1f0d142ad0bad8ce4cef390e1d2a08299ff0f
FHieser <felix_hieser@web.de>
2022-10-19 15:53:58 +0200

    Create implementation Mocks

---
+++ test/utils/mocks/factories/beacon-fundamentals/ImplementationV2Mock.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.0;
+
+import "lib/openzeppelin-contracts/contracts/proxy/utils/Initializable.sol";
+
+contract ImplementationV2Mock is Initializable{
+
+    uint public data;
+
+    constructor(){}
+
+    function initialize(uint _data) external initializer(){
+        data=_data;
+    }
+
+    function getVersion() external pure returns(uint){
+        return 2;
+    }
+
+}
