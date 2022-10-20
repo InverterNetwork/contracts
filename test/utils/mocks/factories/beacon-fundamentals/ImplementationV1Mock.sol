f0f59dba4221457eec58c0e0959b8e14f795e848
FHieser <felix_hieser@web.de>
2022-10-20 17:18:16 +0200

    Adapt Mocks

--- test/utils/mocks/factories/beacon-fundamentals/ImplementationV1Mock.sol
+++ test/utils/mocks/factories/beacon-fundamentals/ImplementationV1Mock.sol
@@ -1,19 +1,18 @@
 pragma solidity ^0.8.0;
 
 import "lib/openzeppelin-contracts/contracts/proxy/utils/Initializable.sol";
+import {ModuleMock} from "test/utils/mocks/modules/base/ModuleMock.sol";
 
-contract ImplementationV1Mock is Initializable{
+contract ImplementationV1Mock is ModuleMock {
+    uint256 public data;
 
-    uint public data;
+    constructor() {}
 
-    constructor(){}
-
-    function initialize(uint _data) external initializer(){
-        data=_data;
+    function initialize(uint256 _data) external initializer {
+        data = _data;
     }
 
-    function getVersion() external pure returns(uint){
+    function getVersion() external pure returns (uint256) {
         return 1;
     }
-
 }
