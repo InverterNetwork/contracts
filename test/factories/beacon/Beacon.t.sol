dbacdaa7d0eb47863b9494a1e86877b94e4c574c
pascal <pascal@merkleplant.xyz>
2022-10-24 11:49:04 +0200

    Beacon, BeaconProxy: Small refactorings

--- test/factories/beacon/Beacon.t.sol
+++ test/factories/beacon/Beacon.t.sol
@@ -19,40 +19,22 @@ import {ImplementationV2Mock} from
 import {OZErrors} from "test/utils/errors/OZErrors.sol";
 
 contract BeaconTest is Test {
+    // SuT
     Beacon beacon;
 
+    // Events copied from SuT
     event Upgraded(address indexed implementation);
-
-    //--------------------------------------------------------------------------------
-    // SETUP
 
     function setUp() public {
         beacon = new Beacon();
     }
 
+    function testDeploymentInvariants() public {
+        assertEq(beacon.implementation(), address(0));
+    }
+
     //--------------------------------------------------------------------------------
-    // TEST MAIN
-
-    function testDeployment() public {
-        assertTrue(beacon.implementation() == address(0));
-    }
-
-    function testUpgradeToOnlyCallableByOwner(address caller) public {
-        vm.assume(caller != address(this));
-        vm.prank(caller);
-
-        vm.expectRevert(OZErrors.Ownable2Step__CallerNotOwner);
-        beacon.upgradeTo(address(0));
-    }
-
-    function testImplemenationIsNotAContract(address implementation) public {
-        if (!Address.isContract(implementation)) {
-            vm.expectRevert(
-                Beacon.Beacon__ImplementationIsNotAContract.selector
-            );
-        }
-        beacon.upgradeTo(implementation);
-    }
+    // Test: upgradeTo
 
     function testUpgradeTo() public {
         ImplementationV1Mock toUpgrade1 = new ImplementationV1Mock();
@@ -63,13 +45,37 @@ contract BeaconTest is Test {
 
         beacon.upgradeTo(address(toUpgrade1));
 
-        assertTrue(beacon.implementation() == address(toUpgrade1));
+        assertEq(beacon.implementation(), address(toUpgrade1));
 
         vm.expectEmit(true, true, true, true);
         emit Upgraded(address(toUpgrade2));
 
         beacon.upgradeTo(address(toUpgrade2));
 
-        assertTrue(beacon.implementation() == address(toUpgrade2));
+        assertEq(beacon.implementation(), address(toUpgrade2));
     }
+
+    function testUpgradeToOnlyCallableByOwner(address caller) public {
+        vm.assume(caller != address(this));
+        vm.prank(caller);
+
+        vm.expectRevert(OZErrors.Ownable2Step__CallerNotOwner);
+        beacon.upgradeTo(address(0));
+    }
+
+    function testUpgradeToFailsIfImplementationNotContract() public {
+        // Note that address(0xCAFE) is EOA.
+        vm.expectRevert(Beacon.Beacon__InvalidImplementation.selector);
+        beacon.upgradeTo(address(0xCAFE));
+    }
+
+    //--------------------------------------------------------------------------------
+    // Test: ERC-165
+
+    function testERC165Supported() public {
+        // @todo felix, mp: Add test to check for following interfaces:
+        //          - ERC 165
+        //          - IBeacon
+    }
+
 }
