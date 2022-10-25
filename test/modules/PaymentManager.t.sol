ae8a5ec26966eb9880b947806aaf35129ad43fca
merkleplant <85061506+pmerkleplant@users.noreply.github.com>
2022-10-25 13:40:59 +0000

    Fixes missing import

--- test/modules/PaymentManager.t.sol
+++ test/modules/PaymentManager.t.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
+// External Interfaces
+import {IERC20} from "@oz/token/ERC20/IERC20.sol";
 
 import {PaymentManager} from "src/modules/PaymentManager.sol";
 // import {ERC20} from "@oz/token/ERC20/ERC20.sol";
