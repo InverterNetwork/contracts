bc1228f00b28a4551cdaaab1bf8afb7d50696d3e
pascal <pascal@merkleplant.xyz>
2022-11-11 16:49:22 +0100

    All: Bump version to ^0.8.13

--- test/modules/ListAuthorizer.t.sol
+++ test/modules/ListAuthorizer.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-only
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
 // @todo mp, nuggan: Sorry, had to refactor ModuleTest contract
 //                   due to faulty initialization.
--- test/utils/mocks/modules/PaymentProcessorMock.sol
+++ test/utils/mocks/modules/PaymentProcessorMock.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-only
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
 import {IPaymentProcessor} from "src/modules/IPaymentProcessor.sol";
 import {IPaymentClient} from "src/modules/mixins/IPaymentClient.sol";
@@ -8,5 +8,7 @@ contract PaymentProcessorMock is IPaymentProcessor {
     //--------------------------------------------------------------------------
     // IPaymentProcessor Functions
 
-    function processPayments(IPaymentClient client) external {}
+    function processPayments(IPaymentClient client)
+        external
+    {}
 }
