83c62d9e6d55baac027ce49e7aaee6f1b68101ae
pascal <pascal@merkleplant.xyz>
2022-11-23 14:46:13 +0100

    PaymentProcessor: Fixes initialization bug
    Closes #118

--- test/utils/mocks/modules/PaymentProcessorMock.sol
+++ test/utils/mocks/modules/PaymentProcessorMock.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.13;
 
+import {IERC20} from "@oz/token/ERC20/IERC20.sol";
+
 import {IPaymentProcessor} from "src/modules/IPaymentProcessor.sol";
 import {IPaymentClient} from "src/modules/mixins/IPaymentClient.sol";
 
@@ -9,4 +11,8 @@ contract PaymentProcessorMock is IPaymentProcessor {
     // IPaymentProcessor Functions
 
     function processPayments(IPaymentClient client) external {}
+
+    function token() external pure returns (IERC20) {
+        return IERC20(address(0));
+    }
 }
