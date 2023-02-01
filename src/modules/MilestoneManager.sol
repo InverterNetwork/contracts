fdc3b54eceaf3487d7be5d9a03d82ed2544c96d8
Rahul Saxena <rahulsaxena.pro@gmail.com>
2023-02-01 10:43:56 +0530

    Implemented suggestions on PR

--- src/modules/MilestoneManager.sol
+++ src/modules/MilestoneManager.sol
@@ -216,8 +216,17 @@ contract MilestoneManager is IMilestoneManager, Module, PaymentClient {
     /// @inheritdoc IMilestoneManager
     function isNextMilestoneActivatable() public view returns (bool) {
         // Return false if next milestone does not exist.
-        uint next = _milestones[_activeMilestone];
+        uint next = _milestones[_activeMilestone]; // next => id
         if (!isExistingMilestoneId(next)) {
+            return false;
+        }
+
+        Milestone storage nextMilestone = _milestoneRegistry[next];
+
+        if (
+            block.timestamp - nextMilestone.lastUpdatedTimestamp
+                < _milestoneUpdateTimelock
+        ) {
             return false;
         }
 
@@ -226,12 +235,7 @@ contract MilestoneManager is IMilestoneManager, Module, PaymentClient {
             return true;
         }
 
-        Milestone storage m = _milestoneRegistry[_activeMilestone];
-
-        if (block.timestamp - m.lastUpdatedTimestamp < _milestoneUpdateTimelock)
-        {
-            return false;
-        }
+        Milestone storage m = _milestoneRegistry[_activeMilestone]; // _milestone
 
         // Milestone is activatable if current milestone started and its
         // duration exceeded.
@@ -296,7 +300,7 @@ contract MilestoneManager is IMilestoneManager, Module, PaymentClient {
             startTimestamp: 0,
             submissionData: "",
             completed: false,
-            lastUpdatedTimestamp: block.timestamp
+            lastUpdatedTimestamp: 0
         });
 
         emit MilestoneAdded(id, duration, budget, title_, details);
