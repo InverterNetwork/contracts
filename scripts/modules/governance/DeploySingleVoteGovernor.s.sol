dfd027dbb17f4aefb3b549a70d88c6650d2e6ee8
0xNuggan <nugganite@outlook.com>
2023-03-22 10:48:31 +0100

    fix URLs

---
+++ scripts/modules/governance/DeploySingleVoteGovernor.s.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+
+import {SingleVoteGovernor} from "../../../src/modules/governance/SingleVoteGovernor.sol";
+
+ /**
+  * @title SingleVoteGovernor Deployment Script
+  *
+  * @dev Script to deploy a new SingleVoteGovernor.
+  *
+  *
+  * @author byterocket
+  */
+
+contract DeploySingleVoteGovernor is Script {
+
+    SingleVoteGovernor singleVoteGovernor;
+
+    function run() external {
+
+        // Deploy the singleVoteGovernor.
+        vm.startBroadcast();
+        {
+            singleVoteGovernor = new SingleVoteGovernor();
+        }
+        vm.stopBroadcast();
+
+        // Log the deployed SingleVoteGovernor contract address.
+        console2.log("Deployment of SingleVoteGovernor at address",
+            address(singleVoteGovernor));
+    }
+
+}
--- tasks/deployBaseContracts.sh
+++ tasks/deployBaseContracts.sh
@@ -1,57 +1,57 @@
 # Script to deploy Inverter Base Contracts
 
 # - ProposalFactory
-forge script scripts/DeployProposalFactory.s.sol \
-    --rpc-url $RPC_URL \
+forge script scripts/factories/DeployProposalFactory.s.sol:DeployProposalFactory \
+    --fork-url $RPC_URL \
     --sender $WALLET_DEPLOYER \
     --private-key $WALLET_DEPLOYER_PK \
     --broadcast
 
 # - ModuleFactory
-forge script scripts/DeployModuleFactory.s.sol \
-    --rpc-url $RPC_URL \
+forge script scripts/factories/DeployModuleFactory.s.sol:DeployModuleFactory \
+    --fork-url $RPC_URL \
     --sender $WALLET_DEPLOYER \
     --private-key $WALLET_DEPLOYER_PK \
     --broadcast
 
 # - Proposal
-forge script scripts/DeployProposal.s.sol \
-    --rpc-url $RPC_URL \
+forge script scripts/proposal/DeployProposal.s.sol:DeployProposal \
+    --fork-url $RPC_URL \
     --sender $WALLET_DEPLOYER \
     --private-key $WALLET_DEPLOYER_PK \
     --broadcast
 
 # - ListAuthorizer
-forge script scripts/DeployListAuthorizer.s.sol \
-    --rpc-url $RPC_URL \
+forge script scripts/modules/governance/DeployListAuthorizer.s.sol:DeployListAuthorizer \
+    --fork-url $RPC_URL \
     --sender $WALLET_DEPLOYER \
     --private-key $WALLET_DEPLOYER_PK \
     --broadcast
 
 # - SingleVoteGovernance
-forge script scripts/DeploySingleVoteGovernance.s.sol \
-    --rpc-url $RPC_URL \
+forge script scripts/modules/governance/DeploySingleVoteGovernor.s.sol:DeploySingleVoteGovernor \
+    --fork-url $RPC_URL \
     --sender $WALLET_DEPLOYER \
     --private-key $WALLET_DEPLOYER_PK \
     --broadcast
 
 # - MilestoneManager
-forge script scripts/DeployMilestoneManager.s.sol \
-    --rpc-url $RPC_URL \
+forge script scripts/modules/DeployMilestoneManager.s.sol:DeployMilestoneManager \
+    --fork-url $RPC_URL \
     --sender $WALLET_DEPLOYER \
     --private-key $WALLET_DEPLOYER_PK \
     --broadcast
 
 # - PaymentManager
 forge script scripts/DeployPaymentManager.s.sol \
-    --rpc-url $RPC_URL \
+    --fork-url $RPC_URL \
     --sender $WALLET_DEPLOYER \
     --private-key $WALLET_DEPLOYER_PK \
     --broadcast
 
 # - Beacon
-forge script scripts/DeployBeacon.s.sol \
-    --rpc-url $RPC_URL \
+forge script scripts/proxies/DeployBeacon.s.sol:DeployBeacon \
+    --fork-url $RPC_URL \
     --sender $WALLET_DEPLOYER \
     --private-key $WALLET_DEPLOYER_PK \
     --broadcast
