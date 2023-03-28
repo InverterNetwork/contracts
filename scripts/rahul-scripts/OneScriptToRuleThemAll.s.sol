51c82b5d144bc9d1d3fbc35bdd729dce36e5784c
Rahul Saxena <rahulsaxena.pro@gmail.com>
2023-03-28 20:12:24 +0530

    Exact replica of setup of E2eTest.sol

--- scripts/rahul-scripts/OneScriptToRuleThemAll.s.sol
+++ scripts/rahul-scripts/OneScriptToRuleThemAll.s.sol
@@ -2,13 +2,20 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import {IModule} from "src/modules/base/IModule.sol";
 import {Proposal} from "../../src/proposal/Proposal.sol";
 import {PaymentProcessor} from "../../src/modules/PaymentProcessor.sol";
 import {MilestoneManager} from "../../src/modules/MilestoneManager.sol";
 
+import {ModuleFactory} from "../../src/factories/ModuleFactory.sol";
+import {ProposalFactory, IProposalFactory} from "src/factories/ProposalFactory.sol";
+
+import {AuthorizerMock} from "../../test/utils/mocks/modules/AuthorizerMock.sol";
+
 import {Beacon, IBeacon} from "../../src/factories/beacon/Beacon.sol";
 
 contract DeployMilestoneManagerContract is Script {
+    address deployer = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
     address paymentProcessorBeaconOwner = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
     address milestoneManagerBeaconOwner = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
     address authorizerBeaconOwner = 0x90F79bf6EB2c4f870365E785982E1f101E93b906;
@@ -16,22 +23,38 @@ contract DeployMilestoneManagerContract is Script {
     Proposal proposal;
     PaymentProcessor paymentProcessor;
     MilestoneManager milestoneManager;
+    ModuleFactory moduleFactory;
+    ProposalFactory proposalFactory;
+
+    AuthorizerMock authorizer;
 
     Beacon paymentProcessorBeacon;
     Beacon milestoneManagerBeacon;
     Beacon authorizerBeacon;
+
+    IModule.Metadata paymentProcessorMetadata = IModule.Metadata( 1, 1, "https://github.com/inverter/payment-processor", "PaymentProcessor");
+    IProposalFactory.ModuleConfig paymentProcessorFactoryConfig = IProposalFactory.ModuleConfig(paymentProcessorMetadata, bytes(""));
+
+    IModule.Metadata milestoneManagerMetadata = IModule.Metadata(1, 1, "https://github.com/inverter/milestone-manager", "MilestoneManager");
+    IProposalFactory.ModuleConfig milestoneManagerFactoryConfig = IProposalFactory.ModuleConfig(milestoneManagerMetadata, abi.encode(100_000_000, 1_000_000, makeAddr("treasury")));
     
-    function run() public {
-        uint256 deployerPrivateKey = vm.envUint("ANVIL_PRIVATE_KEY");
-        uint256 paymentProcessorBeaconOwnerPrivateKey = vm.envUint("PPBO_PRIVATE_KEY");
-        uint256 milestoneManagerBeaconOwnerPrivateKey = vm.envUint("MMBO_PRIVATE_KEY");
-        uint256 authorizerBeaconOwnerPrivateKey = vm.envUint("ABO_PRIVATE_KEY");
-        
+    IModule.Metadata authorizerMetadata = IModule.Metadata(1, 1, "https://github.com/inverter/authorizer", "Authorizer");
+    IProposalFactory.ModuleConfig authorizerFactoryConfig = IProposalFactory.ModuleConfig(authorizerMetadata, abi.encode(address(this)));
+
+    uint256 deployerPrivateKey = vm.envUint("ANVIL_PRIVATE_KEY");
+    uint256 paymentProcessorBeaconOwnerPrivateKey = vm.envUint("PPBO_PRIVATE_KEY");
+    uint256 milestoneManagerBeaconOwnerPrivateKey = vm.envUint("MMBO_PRIVATE_KEY");
+    uint256 authorizerBeaconOwnerPrivateKey = vm.envUint("ABO_PRIVATE_KEY");
+
+    function run() public {        
         vm.startBroadcast(deployerPrivateKey);
         {
             proposal = new Proposal();
             paymentProcessor = new PaymentProcessor();
             milestoneManager = new MilestoneManager();
+            authorizer = new AuthorizerMock();
+            moduleFactory = new ModuleFactory();
+            proposalFactory = new ProposalFactory(address(proposal), address(moduleFactory));
         }
         vm.stopBroadcast();
 
@@ -53,7 +76,24 @@ contract DeployMilestoneManagerContract is Script {
         }
         vm.stopBroadcast();
 
+        // Let us set beacon's implementations.
+        vm.prank(paymentProcessorBeaconOwner);
+        paymentProcessorBeacon.upgradeTo(address(paymentProcessor));
+        
+        vm.prank(milestoneManagerBeaconOwner);
+        milestoneManagerBeacon.upgradeTo(address(milestoneManager));
+        
+        vm.prank(authorizerBeaconOwner);
+        authorizerBeacon.upgradeTo(address(authorizer));
 
+        // Register modules at moduleFactory.
+        vm.startPrank(deployer);
+        moduleFactory.registerMetadata(paymentProcessorMetadata, IBeacon(paymentProcessorBeacon));
+        moduleFactory.registerMetadata(milestoneManagerMetadata, IBeacon(milestoneManagerBeacon));
+        moduleFactory.registerMetadata(authorizerMetadata, IBeacon(authorizerBeacon));
+        vm.stopPrank();
+
+        // Logging the deployed addresses
         console2.log("Proposal Contract Deployed at: ", address(proposal));
         console2.log("Payment Processor Contract Deployed at: ", address(paymentProcessor));
         console2.log("Milestone Manager Contract Deployed at: ", address(milestoneManager));
