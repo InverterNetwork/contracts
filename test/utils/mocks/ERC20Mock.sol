3e8f12b28efb75d126dc8f817a4d3ba42e9e684d
SchneiderNejc <nejc.sch@gmail.com>
2023-04-05 19:04:22 +0200

    VPP3: update ERC20Mock with ability to block addresses

--- test/utils/mocks/ERC20Mock.sol
+++ test/utils/mocks/ERC20Mock.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.13;
 import {ERC20} from "@oz/token/ERC20/ERC20.sol";
 
 contract ERC20Mock is ERC20 {
+    mapping(address => bool) blockedAddresses;
+
     constructor(string memory _name, string memory _symbol)
         ERC20(_name, _symbol)
     {}
@@ -14,5 +16,36 @@ contract ERC20Mock is ERC20 {
 
     function burn(address from, uint value) public {
         _burn(from, value);
+    }
+
+    function blockAddress(address user) public {
+        blockedAddresses[user] = true;
+    }
+
+    function unblockAddress(address user) public {
+        blockedAddresses[user] = false;
+    }
+
+    function isBlockedAddress(address user) public returns(bool) {
+        return blockedAddresses[user];
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        require(!isBlockedAddress(to), "address blocked");
+        address owner = _msgSender();
+        _transfer(owner, to, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
+        require(!isBlockedAddress(to), "address blocked");
+        address spender = _msgSender();
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
+        return true;
     }
 }
