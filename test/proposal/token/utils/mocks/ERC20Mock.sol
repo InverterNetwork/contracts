4e3088d38578aa490ae03e5c83e5ce2f966a93b2
Marvin Kruse <marvinkruse@users.noreply.github.com>
2023-04-13 11:04:52 +0200

    #192 Add ElasticReceiptToken to Repo (#219)
    * Add elastic receipt token contracts
    
    * Update code to use new contracts
    
    * Add tests of elastic receipt token
    
    * Remove lib import
    
    * Update formatting to conform to std

---
+++ test/proposal/token/utils/mocks/ERC20Mock.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.10;
+
+import {ERC20} from "@oz/token/ERC20/ERC20.sol";
+
+contract ERC20Mock is ERC20 {
+    constructor(string memory _name, string memory _symbol)
+        ERC20(_name, _symbol)
+    {
+        // NO-OP
+    }
+
+    function mint(address to, uint value) public virtual {
+        _mint(to, value);
+    }
+
+    function burn(address from, uint value) public virtual {
+        _burn(from, value);
+    }
+}
