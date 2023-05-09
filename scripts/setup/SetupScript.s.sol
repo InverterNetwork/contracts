8753121ca0c6f7449a2b11a49728672c46400db5
Rahul Saxena <rahulsaxena.pro@gmail.com>
2023-05-09 12:40:10 +0530

    A new test proposal is created

--- scripts/setup/SetupScript.s.sol
+++ scripts/setup/SetupScript.s.sol
@@ -30,34 +30,38 @@ contract SetupScript is Script, DeploymentScript {
 
     address funder1 = address(0xF1);
     address funder2 = address(0xF2);
+
     address proposalOwner = 0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65;
-    
     uint256 proposalOwnerPrivateKey = vm.envUint("PROPOSAL_OWNER_PRIVATE_KEY");
 
     function run() public override {
+        ERC20Mock token;
         DeploymentScript.run();
 
-        // vm.startBroadcast(deployerPrivateKey);
-        // ERC20Mock token = new ERC20Mock("Mock", "MOCK");
-        // vm.stopBroadcast();
+        vm.startBroadcast(deployerPrivateKey);
+        {
+            token = new ERC20Mock("Mock", "MOCK");
+        }
+        vm.stopBroadcast();
 
-        // // First, we create a new proposal.
-        // IProposalFactory.ProposalConfig memory proposalConfig = IProposalFactory.ProposalConfig({
-        //     owner: proposalOwner, 
-        //     token: token
-        // });
+        // First, we create a new proposal.
+        IProposalFactory.ProposalConfig memory proposalConfig = IProposalFactory.ProposalConfig({
+            owner: proposalOwner, 
+            token: token
+        });
 
-        // IProposalFactory.ModuleConfig[] memory optionalModules = new IProposalFactory.ModuleConfig[](1);
-        // optionalModules[0] = milestoneManagerFactoryConfig;
+        IProposalFactory.ModuleConfig[] memory optionalModules = new IProposalFactory.ModuleConfig[](1);
+        optionalModules[0] = milestoneManagerFactoryConfig;
 
-        // IProposal test_proposal = proposalFactory.createProposal(
-        //                                                 proposalConfig,
-        //                                                 authorizerFactoryConfig,
-        //                                                 paymentProcessorFactoryConfig,
-        //                                                 optionalModules
-        //                                             );
+        IProposal test_proposal = proposalFactory.createProposal(
+                                                        proposalConfig,
+                                                        authorizerFactoryConfig,
+                                                        paymentProcessorFactoryConfig,
+                                                        optionalModules
+                                                    );
         
-        // console2.log("ERC20 token address: ", address(token));
+        console2.log("ERC20 token address: ", address(token));
+        console2.log("Test Proposal Address: ", address(test_proposal));
     }
 
 }
