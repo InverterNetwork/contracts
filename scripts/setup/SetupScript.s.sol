463a49ef7e08dc897780710b2c257d77d940fc0b
Rahul Saxena <rahulsaxena.pro@gmail.com>
2023-05-16 16:41:04 +0530

    Setup scripts works now with the latest changes from main

--- scripts/setup/SetupScript.s.sol
+++ scripts/setup/SetupScript.s.sol
@@ -1,11 +1,15 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// import "forge-std/Script.sol";
-// import "forge-std/Test.sol";
-// import "forge-std/StdJson.sol";
+import "forge-std/Script.sol";
+import "forge-std/Test.sol";
+import "forge-std/StdJson.sol";
 
-// import "../deployment/DeploymentScript.s.sol";
+import "../deployment/DeploymentScript.s.sol";
+
+import {IMilestoneManager} from "../../src/modules/MilestoneManager.sol";
+import {IProposal} from "../../src/proposal/Proposal.sol";
+import {ERC20Mock} from "../../test/utils/mocks/ERC20Mock.sol";
 
 // import {IMilestoneManager} from "../../src/modules/MilestoneManager.sol";
 // import {IProposal} from "../../src/proposal/Proposal.sol";
@@ -19,36 +23,36 @@ contract SetupScript is Test, Script, DeploymentScript {
         // 1. A non-empty list of contributors for it
         // 2. The percentage of milestone funding to pay the contributors for the milestone.
 
-//         // So lets add Alice and Bob as contributors to the proposal.
-//         // Note the salary is specified in relation to the SALARY_PRECISION variable in the MilestoneManager.
-//     */
+        // So lets add Alice and Bob as contributors to the proposal.
+        // Note the salary is specified in relation to the SALARY_PRECISION variable in the MilestoneManager.
+    */
 
-//     IMilestoneManager.Contributor alice = IMilestoneManager.Contributor(
-//         address(0xA11CE), 50_000_000, "AliceIdHash"
-//     );
+    IMilestoneManager.Contributor alice = IMilestoneManager.Contributor(
+        address(0xA11CE), 50_000_000, "AliceIdHash"
+    );
 
     IMilestoneManager.Contributor bob =
         IMilestoneManager.Contributor(address(0x606), 50_000_000, "BobIdHash");
 
-//     IMilestoneManager.Contributor[] contributors;
+    IMilestoneManager.Contributor[] contributors;
 
-//     address funder1 = address(0xF1);
-//     address funder2 = address(0xF2);
+    address funder1 = address(0xF1);
+    address funder2 = address(0xF2);
 
     address proposalOwner = 0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65;
     uint proposalOwnerPrivateKey = vm.envUint("PROPOSAL_OWNER_PRIVATE_KEY");
 
-//     function run() public override {
-//         ERC20Mock token;
-//         IProposal test_proposal;
+    function run() public override {
+        ERC20Mock token;
+        IProposal test_proposal;
 
-//         // DeploymentScript.run();
+        DeploymentScript.run();
 
-//         vm.startBroadcast(deployerPrivateKey);
-//         {
-//             token = new ERC20Mock("Mock", "MOCK");
-//         }
-//         vm.stopBroadcast();
+        vm.startBroadcast(deployerPrivateKey);
+        {
+            token = new ERC20Mock("Mock", "MOCK");
+        }
+        vm.stopBroadcast();
 
         // First, we create a new proposal.
         IProposalFactory.ProposalConfig memory proposalConfig = IProposalFactory
@@ -67,8 +71,8 @@ contract SetupScript is Test, Script, DeploymentScript {
         );
         vm.stopPrank();
 
-//         console2.log("Proposal Contract", address(test_proposal));
-//         assert(address(test_proposal) != address(0));
+        console2.log("Proposal Contract", address(test_proposal));
+        assert(address(test_proposal) != address(0));
 
         address proposalToken = address(IProposal(test_proposal).token());
         assertEq(proposalToken, address(token));
@@ -119,14 +123,14 @@ contract SetupScript is Test, Script, DeploymentScript {
             // Tx1559Detail memory txDetail = rawToConvertedEIP1559Detail(rawTxDetail);
             // assertEq(txDetail.from, makeAddr("Beef"));
 
-//             uint index = 0;
-//             Receipt memory receipt = readReceipt("broadcast/SetupScript.s.sol/31337/run-latest.json", index);
-//             console2.log("Contract Address", receipt.contractAddress);
-//             //assertEq(receipt.contractAddress, address(0));
+            uint index = 0;
+            Receipt memory receipt = readReceipt("broadcast/SetupScript.s.sol/31337/run-latest.json", index);
+            console2.log("Contract Address", receipt.contractAddress);
+            //assertEq(receipt.contractAddress, address(0));
 
-//             // console2.log(latestRunJson);
-//             //console2.log("Transaction 2", transactions[2].contractName);
-//         */
+            // console2.log(latestRunJson);
+            //console2.log("Transaction 2", transactions[2].contractName);
+        */
 
         vm.startPrank(address(proposalOwner));
         proposalCreatedMilestoneManager.addMilestone(
@@ -155,44 +159,44 @@ contract SetupScript is Test, Script, DeploymentScript {
 
         vm.stopPrank();
 
-//         vm.startPrank(address(proposalOwner));
-//         proposalCreatedMilestoneManager.addMilestone(
-//             1 weeks,
-//             1000e18,
-//             contributors,
-//             bytes("Here could be a more detailed description")
-//         );
+        vm.startPrank(address(proposalOwner));
+        proposalCreatedMilestoneManager.addMilestone(
+            1 weeks,
+            1000e18,
+            contributors,
+            bytes("Here could be a more detailed description")
+        );
 
-//         proposalCreatedMilestoneManager.addMilestone(
-//             2 weeks,
-//             5000e18,
-//             contributors,
-//             bytes("The second milestone, right after the first one")
-//         );
+        proposalCreatedMilestoneManager.addMilestone(
+            2 weeks,
+            5000e18,
+            contributors,
+            bytes("The second milestone, right after the first one")
+        );
 
-//         // IMPORTANT
-//         // =========
-//         // Due to how the underlying rebase mechanism works, it is necessary
-//         // to always have some amount of tokens in the proposal.
-//         // It's best, if the owner deposits them right after deployment.
-//         uint initialDeposit = 10e18;
-//         token.mint(address(proposalOwner), initialDeposit);
-//         token.approve(address(test_proposal), initialDeposit);
-//         test_proposal.deposit(initialDeposit);
+        // IMPORTANT
+        // =========
+        // Due to how the underlying rebase mechanism works, it is necessary
+        // to always have some amount of tokens in the proposal.
+        // It's best, if the owner deposits them right after deployment.
+        uint initialDeposit = 10e18;
+        token.mint(address(proposalOwner), initialDeposit);
+        token.approve(address(test_proposal), initialDeposit);
+        test_proposal.deposit(initialDeposit);
 
-//         vm.stopPrank();
+        vm.stopPrank();
 
         // Let's confirm whether the milestone was added or not.
         // milestoneId 1 should exist and 0 shouldn't, since IDs start from 1.
         assertTrue(!(proposalCreatedMilestoneManager.isExistingMilestoneId(0)));
         assertTrue(proposalCreatedMilestoneManager.isExistingMilestoneId(1));
 
-//         assertTrue(proposalCreatedMilestoneManager.isContributor(1, alice.addr));
-//         assertTrue(proposalCreatedMilestoneManager.isContributor(2, alice.addr));
+        assertTrue(proposalCreatedMilestoneManager.isContributor(1, alice.addr));
+        assertTrue(proposalCreatedMilestoneManager.isContributor(2, alice.addr));
 
-//         // Seeing this great working on the proposal, funder1 decides to fund
-//         // the proposal with 1k of tokens.
-//         token.mint(funder1, 1000e18);
+        // Seeing this great working on the proposal, funder1 decides to fund
+        // the proposal with 1k of tokens.
+        token.mint(funder1, 1000e18);
 
         vm.startPrank(funder1);
         {
