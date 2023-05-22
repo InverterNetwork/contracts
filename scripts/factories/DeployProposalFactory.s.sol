841ef4a48231869ae4c64461fa3dd4a2997092fb
Rahul Saxena <rahulsaxena.pro@gmail.com>
2023-05-22 16:45:55 +0530

    removed relative paths from scripts

--- scripts/factories/DeployProposalFactory.s.sol
+++ scripts/factories/DeployProposalFactory.s.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
-import {ProposalFactory} from "../../src/factories/ProposalFactory.sol";
+import {ProposalFactory} from "src/factories/ProposalFactory.sol";
 
 /**
  * @title ProposalFactory Deployment Script
