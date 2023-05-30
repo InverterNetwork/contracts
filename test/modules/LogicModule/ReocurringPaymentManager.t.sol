3e899d8762bd60be6d3f5eaf3835a54f32988491
FHieser <felix_hieser@web.de>
2023-05-30 15:44:39 +0200

    Test Modifier and create basic time warp setup

--- test/modules/LogicModule/ReocurringPaymentManager.t.sol
+++ test/modules/LogicModule/ReocurringPaymentManager.t.sol
@@ -24,7 +24,6 @@ contract ReocurringPaymentManagerTest is ModuleTest {
 
     function setUp() public {
         //Add Module to Mock Proposal
-
         address impl = address(new ReocurringPaymentManager());
         reocurringPaymentManager = ReocurringPaymentManager(Clones.clone(impl));
 
@@ -47,7 +46,7 @@ contract ReocurringPaymentManagerTest is ModuleTest {
             _proposal, _METADATA, abi.encode(1 weeks - 1)
         );
 
-        //Init Module wrongly
+        //Init Module correct
         reocurringPaymentManager.init(_proposal, _METADATA, abi.encode(1 weeks));
 
         assertEq(reocurringPaymentManager.getEpochLength(), 1 weeks);
@@ -60,5 +59,78 @@ contract ReocurringPaymentManagerTest is ModuleTest {
         reocurringPaymentManager.init(_proposal, _METADATA, bytes(""));
     }
 
+    //--------------------------------------------------------------------------
+    // Modifier
+
+    function testValidId(uint seed, uint usedIds, uint id) public {
+        vm.assume(usedIds < 1000); //Reasonable size
+
+        reasonableWarpAndInit(seed);
+
+        for (uint i = 0; i < usedIds; i++) {
+            reocurringPaymentManager.addReocurringPayment(
+                1, 3 weeks, address(0xBEEF)
+            );
+        }
+
+        if (id > usedIds || id == 0) {
+            vm.expectRevert(
+                IReocurringPaymentManager
+                    .Module__ReocurringPaymentManager__InvalidReocurringPaymentId
+                    .selector
+            );
+        }
+
+        reocurringPaymentManager.getReocurringPaymentInformation(id);
+    }
+
+    function testValidStartEpoch(uint seed, uint startEpoch) public {
+        reasonableWarpAndInit(seed);
+
+        uint currentEpoch = reocurringPaymentManager.getCurrentEpoch();
+
+        if (currentEpoch > startEpoch) {
+            vm.expectRevert(
+                IReocurringPaymentManager
+                    .Module__ReocurringPaymentManager__InvalidStartEpoch
+                    .selector
+            );
+        }
+
+        reocurringPaymentManager.addReocurringPayment(
+            1, startEpoch, address(0xBeef)
+        );
+    }
+
+    //--------------------------------------------------------------------------
+    // Getter
+
+    function testGetReocurringPaymentInformationModifierInPosition() public {
+        vm.expectRevert(
+            IReocurringPaymentManager
+                .Module__ReocurringPaymentManager__InvalidReocurringPaymentId
+                .selector
+        );
+        reocurringPaymentManager.getReocurringPaymentInformation(0);
+    }
+
     // =========================================================================
+
+    //--------------------------------------------------------------------------
+    // Helper
+
+    function reasonableWarpAndInit(uint seed) internal {
+        uint epochLength = bound(seed, 1 weeks, 52 weeks);
+
+        //with this were at least in epoch 2 and there is enough time to go on from that time (3_153_600_000 seconds are 100 years)
+        uint currentTimestamp = bound(seed, 52 weeks + 1, 3_153_600_000);
+
+        //Warp to a reasonable time
+        vm.warp(currentTimestamp);
+
+        //Init Module
+        reocurringPaymentManager.init(
+            _proposal, _METADATA, abi.encode(epochLength)
+        );
+    }
 }
