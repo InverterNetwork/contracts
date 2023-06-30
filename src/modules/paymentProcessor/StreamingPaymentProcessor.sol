4e91ab82bc6f3311d1a34053261f21709e279766
Rahul Saxena <rahulsaxena.pro@gmail.com>
2023-06-30 14:56:39 +0530

    improved the faulty usage of dueTo as a duration rather than a timestamp

--- src/modules/paymentProcessor/StreamingPaymentProcessor.sol
+++ src/modules/paymentProcessor/StreamingPaymentProcessor.sol
@@ -448,11 +448,7 @@ contract StreamingPaymentProcessor is Module, IStreamingPaymentProcessor {
 
             // If the paymentOrder being removed was already past its duration, then it would have been removed in the earlier _claimForSpecificWalletId call
             // Otherwise, we would remove that paymentOrder in the following lines.
-            if (
-                block.timestamp
-                    < startForSpecificWalletId(client, contributor, walletId)
-                        + dueToForSpecificWalletId(client, contributor, walletId)
-            ) {
+            if ( block.timestamp < dueToForSpecificWalletId(client, contributor, walletId)) {
                 _removePaymentForSpecificWalletId(client, contributor, walletId);
 
                 _removeVestingInformationForSpecificWalletId(
