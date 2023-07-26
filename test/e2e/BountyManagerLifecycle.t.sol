45347d9e6b7cab758bb965c93c2040c8801cdf78
0xNuggan <82726722+0xNuggan@users.noreply.github.com>
2023-07-26 15:58:06 +0200

    add e2e tests (#303)
    * add e2e tests
    
    * remove comment

--- test/e2e/BountyManagerLifecycle.t.sol
+++ test/e2e/BountyManagerLifecycle.t.sol
@@ -53,7 +53,10 @@ contract BountyManagerLifecycle is E2eTest {
         IProposalFactory.ProposalConfig memory proposalConfig = IProposalFactory
             .ProposalConfig({owner: address(this), token: token});
 
-        IProposal proposal = _createNewProposalWithAllModules(proposalConfig);
+        IProposal proposal =
+        _createNewProposalWithAllModules_withRoleBasedAuthorizerAndBountyManager(
+            proposalConfig
+        );
 
         RebasingFundingManager fundingManager =
             RebasingFundingManager(address(proposal.fundingManager()));
@@ -72,5 +75,78 @@ contract BountyManagerLifecycle is E2eTest {
                 continue;
             }
         }
+
+        bountyManager.toggleSelfManagement();
+
+        // we authorize the deployer of the proposal as the bounty admin
+        bountyManager.grantBountyAdminRole(address(this));
+        // Funders deposit funds
+
+        // IMPORTANT
+        // =========
+        // Due to how the underlying rebase mechanism works, it is necessary
+        // to always have some amount of tokens in the proposal.
+        // It's best, if the owner deposits them right after deployment.
+        uint initialDeposit = 10e18;
+        token.mint(address(this), initialDeposit);
+        token.approve(address(fundingManager), initialDeposit);
+        fundingManager.deposit(initialDeposit);
+
+        // Seeing this great working on the proposal, funder1 decides to fund
+        // the proposal with 1k of tokens.
+        address funder1 = makeAddr("funder1");
+
+        token.mint(funder1, 1000e18);
+
+        vm.startPrank(funder1);
+        {
+            token.approve(address(fundingManager), 1000e18);
+            fundingManager.deposit(1000e18);
+        }
+        vm.stopPrank();
+
+        // Owner sets up bounty
+        uint minimumPayoutAmount = 100e18;
+        uint maximumPayoutAmount = 500e18;
+        bytes memory details = "This is a test bounty";
+
+        uint bountyId = bountyManager.addBounty(
+            minimumPayoutAmount, maximumPayoutAmount, details
+        );
+
+        // Workers submit bounty
+        IBountyManager.Contributor memory contrib1 =
+            IBountyManager.Contributor(address(0xA11CE), 150e18);
+        IBountyManager.Contributor memory contrib2 =
+            IBountyManager.Contributor(address(0xb0b), 150e18);
+
+        //auth.setIsAuthorized(address(0xA11CE), true);
+        bountyManager.grantClaimAdminRole(address(0xA11CE));
+
+        IBountyManager.Contributor[] memory contribs =
+            new IBountyManager.Contributor[](2);
+        contribs[0] = contrib1;
+        contribs[1] = contrib2;
+
+        bytes memory claimDetails = "This is a test submission";
+
+        vm.prank(contrib1.addr);
+        uint claimId = bountyManager.addClaim(1, contribs, claimDetails);
+
+        // Verifiers approve bounty
+
+        address verifier1 = makeAddr("verifier 1");
+
+        //auth.setIsAuthorized(verifier1, true);
+        bountyManager.grantVerifyAdminRole(verifier1);
+
+        vm.prank(verifier1);
+        bountyManager.verifyClaim(claimId, bountyId);
+
+        // Bounty has been paid out
+        assertEq(token.balanceOf(contrib1.addr), 150e18);
+        assertEq(token.balanceOf(contrib2.addr), 150e18);
+
+        // TODO: Update with real roleAuthorizer
     }
 }
