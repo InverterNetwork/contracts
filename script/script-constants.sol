ddcf55eeec36e57d0418a2606c4dd2dc2683988a
Rahul Saxena <rahulsaxena.pro@gmail.com>
2023-09-12 11:59:51 +0530

    added constants from deployTokenAuthorizerAndSwitch to constants file

--- script/script-constants.sol
+++ script/script-constants.sol
@@ -6,12 +6,14 @@ contract ScriptConstants {
     // Common Constants
     ///////////////////////
     bytes public emptyBytes = "0x0";
+    address public bountyManagerAddress = 0x4FB5adc63fB08c7E7864Ce3f77714af6B8B50D9f;
+    address public moduleFactoryAddress = 0x349D52589aF62Ba1b35DB871F54FA2c5aFcA6B5B;
+    address public orchestratorAddress = 0x0A7c8C0EB1afAb6CBaD4bb2d4c738acFF047814A;
+    address public receiptTokenAddress = 0xC0f1842627Eeda938911A9A8368407ec241AC1dd;
 
     ////////////////////////////////////////
     // Add Bounty Manager Claim Constants
     ////////////////////////////////////////
-    address public bountyManagerAddress = 0x4FB5adc63fB08c7E7864Ce3f77714af6B8B50D9f;
-    
     address public addBountyManagerClaim_user1 = 0x9518a55e5cd4Ac650A37a6Ab6c352A3146D2C9BD;
     address public addBountyManagerClaim_user2 = 0x3064A400b5e74BeA12391058930EfD95a6911970;
 
