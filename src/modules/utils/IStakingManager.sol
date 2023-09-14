1f5d96bd6bc02d267b6753b0404eed09065d5210
FHieser <felix_hieser@web.de>
2023-09-14 12:41:56 +0200

    Basic implementation

---
+++ src/modules/utils/IStakingManager.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.0;
+
+// External Interfaces
+import {IERC20} from "@oz/token/ERC20/IERC20.sol";
+
+interface IStakingManager {
+    //--------------------------------------------------------------------------
+    // Errors
+
+    struct Stake {
+        uint id;
+        uint amount;
+        uint timesstamp;
+    }
+
+    //--------------------------------------------------------------------------
+    // Events
+
+    /// @notice Event emitted when a deposit takes place.
+    /// @param stakeId The id of the stake that was created.
+    /// @param from The address depositing tokens.
+    /// @param to The address that will receive the receipt tokens.
+    /// @param amount The amount of tokens deposited.
+    event Deposit(uint stakeId, address from, address to, uint amount);
+
+    /// @notice Event emitted when a withdrawal takes place.
+    /// @param stakeId The id of the stake that was withdrawn from.
+    /// @param from The address supplying the receipt tokens.
+    /// @param to The address that will receive the underlying tokens.
+    /// @param amount The amount of underlying tokens withdrawn.
+    event Withdrawal(uint stakeId, address from, address to, uint amount);
+
+    //--------------------------------------------------------------------------
+    // Functions
+
+    //Getter Functions
+    function token() external view returns (IERC20);
+
+    function getTotalAmount() external view returns (uint);
+
+    function getStakeForAddress(address addr, uint id)
+        external
+        view
+        returns (Stake stake);
+
+    function getAllStakeIdsForAddress(address addr)
+        external
+        view
+        returns (uint[] stakeIds);
+
+    //Mutating Functions
+
+    function deposit(uint amount) external returns (uint stakeId);
+    function depositFor(address to, uint amount)
+        external
+        returns (uint stakeId);
+
+    function withdraw(uint stakeId, uint amount) external;
+    function withdrawTo(uint stakeId, address to, uint amount) external;
+}
---
+++ src/modules/utils/StakingManager.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity 0.8.19;
+
+// Internal Dependencies
+import {Module} from "src/modules/base/Module.sol";
+
+// Internal Interfaces
+import {IStakingManager} from "src/modules/utils/IStakingManager.sol";
+// Internal Libraries
+import {LinkedIdList} from "src/common/LinkedIdList.sol";
+
+// External Interfaces
+import {IERC20} from "@oz/token/ERC20/IERC20.sol";
+// External Libraries
+import {SafeERC20} from "@oz/token/ERC20/utils/SafeERC20.sol";
+
+contract StakingManager is IStakingManager, Module {
+    using SafeERC20 for IERC20;
+    using LinkedIdList for LinkedIdList.List;
+
+    IERC20 private token;
+
+    //--------------------------------------------------------------------------
+    // Storage
+    uint private _totalAmount;
+
+    mapping(address => LinkedIdList.List) private _stakeIds;
+    mapping(address => mapping(uint => Stake)) private _stakeRegistry;
+
+    //--------------------------------------------------------------------------
+    // Initialization
+
+    /// @inheritdoc Module
+    function init(
+        IOrchestrator orchestrator_,
+        Metadata memory metadata,
+        bytes memory configData
+    ) external override(Module) initializer {
+        __Module_init(orchestrator_, metadata);
+
+        token = IERC20(abi.decode(configData, (address)));
+    }
+
+    //--------------------------------------------------------------------------
+    // Functions
+
+    //Getter Functions
+    function token() external view returns (IERC20) {
+        return token;
+    }
+
+    function getTotalAmount() external view returns (uint) {
+        return _totalAmount;
+    }
+
+    function getStakeForAddress(uint id, address addr)
+        external
+        view
+        returns (Stake stake)
+    {
+        return _stakeRegistry[addr][id];
+    }
+
+    function getAllStakeIdsForAddress(address addr)
+        external
+        view
+        returns (uint[] stakeIds)
+    {
+        return _stakeIds[addr].listIds();
+    }
+
+    //Mutating Functions
+    function deposit(uint amount) external returns (uint stakeId) {
+        return _depositFor(_msgSender(), amount);
+    }
+
+    function depositFor(address to, uint amount)
+        external
+        returns (uint stakeId)
+    {
+        return _depositFor(to, amount);
+    }
+
+    function withdraw(uint stakeId, uint amount) external {
+        _withdrawTo(stakeId, _msgSender(), amount);
+    }
+
+    function withdrawTo(uint stakeId, address to, uint amount) external;
+
+    function _depositFor(address to, uint amount)
+        internal
+        returns (uint stakeId)
+    {
+        address sender = _msgSender();
+        token.safeTransferFrom(sender, address(this), amount_);
+        //@todo create stake struct accordingly
+
+        emit Deposit(stakeId, sender, address(this), amount);
+    }
+
+    function _withdrawTo(uint stakeId, address to, uint amount) internal {
+        token.safeTransfer(to, amount);
+        //@todo modify stake struct accordingly
+
+        emit Withdrawal(stakeId, address(this), to, amount);
+    }
+}
