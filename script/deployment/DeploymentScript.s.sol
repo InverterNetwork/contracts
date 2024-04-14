6c2eb93c2c8b21166c2bcd9b7bbd12c31f404618
Marvin Kruse <marvinkruse@users.noreply.github.com>
2024-04-14 16:42:38 +0200

    Fix: Resolve merge conflict with dev

--- script/deployment/DeploymentScript.s.sol
+++ script/deployment/DeploymentScript.s.sol
@@ -392,36 +392,6 @@ contract DeploymentScript is Script {
             moduleFactory,
             metadataManagerMetadata
         );
-        recurringPaymentManagerBeacon = deployAndSetUpBeacon
-            .deployAndRegisterInFactory(
-            recurringPaymentManager,
-            moduleFactory,
-            recurringPaymentManagerMetadata
-        );
-
-        // Utils
-        singleVoteGovernorBeacon = deployAndSetUpBeacon
-            .deployAndRegisterInFactory(
-            singleVoteGovernor, moduleFactory, singleVoteGovernorMetadata
-        );
-        metadataManagerBeacon = deployAndSetUpBeacon.deployAndRegisterInFactory(
-            metadataManager, moduleFactory, metadataManagerMetadata
-        );
-        recurringPaymentManagerBeacon = deployAndSetUpBeacon
-            .deployAndRegisterInFactory(
-            recurringPaymentManager,
-            moduleFactory,
-            recurringPaymentManagerMetadata
-        );
-
-        // Utils
-        singleVoteGovernorBeacon = deployAndSetUpBeacon
-            .deployAndRegisterInFactory(
-            singleVoteGovernor, moduleFactory, singleVoteGovernorMetadata
-        );
-        metadataManagerBeacon = deployAndSetUpBeacon.deployAndRegisterInFactory(
-            metadataManager, moduleFactory, metadataManagerMetadata
-        );
 
         return (orchestratorFactory);
     }
