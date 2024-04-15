08b619d4f02ae6bf855e6556111d273bd1c6ace6
0xNuggan <82726722+0xNuggan@users.noreply.github.com>
2024-04-15 12:23:18 +0200

    Add KPIRewarder files

---
+++ src/modules/logicModule/IKPIRewarder.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.0;
+
+import {
+    IStakingManager,
+    StakingManager,
+    SafeERC20,
+    IERC20,
+    IERC20PaymentClient,
+    ReentrancyGuard
+} from "./StakingManager.sol";
+
+import {
+    IOptimisticOracleIntegrator,
+    OptimisticOracleIntegrator,
+    OptimisticOracleV3CallbackRecipientInterface,
+    OptimisticOracleV3Interface,
+    ClaimData
+} from "./oracle/OptimisticOracleIntegrator.sol";
+
+interface IKPIRewarder {
+    // TODO Natspec
+
+    //--------------------------------------------------------------------------
+    // Types
+
+    /// @notice A KPI to be used for reward distribution
+    struct KPI {
+        /// @dev The number of tranches the KPI is divided into
+        uint numOfTranches; //
+        /// @dev  The total rewards to be distributed
+        uint totalRewards;
+        /// @dev  If the tranche rewards should be distributed continuously or in steps
+        bool continuous;
+        /// @dev The value at which each tranche ends
+        uint[] trancheValues; //
+        /// @dev The rewards to be dsitributed at completion of each tranche
+        uint[] trancheRewards;
+    }
+
+    /// @notice The configuration of the reward round tied
+    struct RewardRoundConfiguration {
+        /// @dev The timestamp the assertion was posted
+        uint creationTime; // timestamp the assertion was created
+        /// @dev The value that was asserted
+        uint assertedValue;
+        /// @dev The KPI to be used for distribution once the assertion confirms
+        uint KpiToUse;
+        /// @dev if the rewards have been distributed or not
+        bool distributed;
+    }
+
+    //--------------------------------------------------------------------------
+    // Errors
+
+    /// @notice The KPI beinge created has either no tranches or too many
+    error Module__KPIRewarder__InvalidTrancheNumber();
+
+    /// @notice The number of tranches in the KPI does not match the number of rewards
+    error Module__KPIRewarder__InvalidKPIValueLengths();
+
+    /// @notice The values for the tranches are not in ascending order
+    error Module__KPIRewarder__InvalidKPITrancheValues();
+
+    /// @notice The KPI number is invalid
+    error Module__KPIRewarder__InvalidKPINumber();
+
+    /// @notice The target value for the assertion cannot be zero
+    error Module__KPIRewarder__InvalidTargetValue();
+
+    /// @notice The Queue for new stakers is full
+    error Module__KPIRewarder__StakingQueueIsFull();
+
+    /// @notice The Token used paying the bond cannot be the same that is being staked.
+    error Module__KPIRewarder__ModuleCannotUseStakingTokenAsBond();
+
+    //--------------------------------------------------------------------------
+    // Events
+
+    /// @notice Event emitted when a user stake is enqueued
+    event StakeEnqueued(address user, uint amount);
+
+    /// @notice Event emitted when a user stake is dequeued before staking
+    event StakeDequeued(address user, uint amount);
+
+    /// @notice Event emitted when a KPI is created
+    event KPICreated(
+        uint KPI_Id,
+        uint numOfTranches,
+        uint totalKPIRewards,
+        bool continuous,
+        uint[] trancheValues,
+        uint[] trancheRewards
+    );
+
+    /// @notice Event emitted when funds for paying the bonding fee are deposited into the contract
+    event FeeFundsDeposited(address token, uint amount);
+
+    //--------------------------------------------------------------------------
+    // Functions
+
+    /// @notice Posts an assertion to the Optimistic Oracle, specifying the KPI to use and the asserted value
+    /// @param dataId The dataId to be posted
+    /// @param data The data to be posted
+    /// @param asserter The address of the asserter
+    /// @param assertedValue The target value that will be asserted
+    /// @param targetKPI The KPI to be used for distribution once the assertion confirms
+    /// @return assertionId The assertionId received for the posted assertion
+    function postAssertion(
+        bytes32 dataId,
+        bytes32 data,
+        address asserter,
+        uint assertedValue,
+        uint targetKPI
+    ) external returns (bytes32 assertionId);
+
+    /// @notice Creates a KPI for the Rewarder
+    /// @param _continuous Should the tranche rewards be distributed continuously or in steps
+    /// @param _trancheValues The value at which the tranches end
+    /// @param _trancheRewards The rewards to be distributed at completion of each tranche
+    function createKPI(
+        bool _continuous,
+        uint[] calldata _trancheValues,
+        uint[] calldata _trancheRewards
+    ) external returns (uint);
+
+    /// @notice Deposits funds into the contract so it can pay for the oracle bond and fee itself
+    /// @param amount The amount to deposit
+    function depositFeeFunds(uint amount) external;
+
+    /// @notice Remove a users funds from the staking queue
+    function dequeueStake() external;
+
+    /// @notice Returns the KPI with the given number
+    /// @param KPInum The number of the KPI to return
+    function getKPI(uint KPInum) external view returns (KPI memory);
+
+    /// @notice Returns the current queue to stake in the contract
+    function getStakingQueue() external view returns (address[] memory);
+
+    /// @notice Returns the Assertion Configuration for a given assertionId
+    /// @param assertionId The id of the Assertion to return
+    function getAssertionConfig(bytes32 assertionId)
+        external
+        view
+        returns (RewardRoundConfiguration memory);
+}
---
+++ src/modules/logicModule/StakingManager.sol
@@ -0,0 +1,307 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity 0.8.23;
+
+// Internal Dependencies
+import {
+    ERC20PaymentClient,
+    Module
+} from "src/modules/logicModule/paymentClient/ERC20PaymentClient.sol";
+
+// Internal Interfaces
+import {IOrchestrator} from "src/orchestrator/IOrchestrator.sol";
+
+import {
+    IERC20PaymentClient,
+    IPaymentProcessor
+} from "src/modules/logicModule/paymentClient/ERC20PaymentClient.sol";
+
+import {IStakingManager} from "src/modules/logicModule/IStakingManager.sol";
+
+// External Interfaces
+import {IERC20} from "@oz/token/ERC20/IERC20.sol";
+
+// External Libraries
+import {SafeERC20} from "@oz/token/ERC20/utils/SafeERC20.sol";
+import {ReentrancyGuard} from "@oz/utils/ReentrancyGuard.sol";
+
+contract StakingManager is
+    IStakingManager,
+    ERC20PaymentClient,
+    ReentrancyGuard
+{
+    using SafeERC20 for IERC20;
+    //--------------------------------------------------------------------------
+    // Modifiers
+
+    modifier validDuration(uint duration) {
+        if (duration == 0) {
+            revert Module__StakingManager__InvalidDuration();
+        }
+        _;
+    }
+
+    //--------------------------------------------------------------------------
+    // Storage
+    /// @dev address of the token that can be staked here
+    address public stakingToken;
+    /// @dev total supply of the token that is staked here
+    uint public totalSupply;
+    /// @dev rate of how many reward tokens are distributed from the fundingmanager to the whole staking pool in seconds
+    uint public rewardRate;
+    /// @dev timestamp of when the reward period will end
+    uint public rewardsEnd;
+    /// @dev internal value that is needed to calculate the reard each user will receive
+    uint internal rewardValue;
+    /// @dev timestamp of when the rewardValue was last updated
+    uint internal lastUpdate;
+
+    /// @dev mapping of balances of each user in the staking token
+    mapping(address => uint) internal _balances;
+    /// @dev mapping of reward Values that are needed to calculate the rewards that a user should receive
+    /// @dev should change everytime the user stakes or unstakes funds
+    mapping(address => uint) internal userRewardValue;
+    /// @dev mapping of how many reward tokens the user accumulated
+    mapping(address => uint) internal rewards;
+
+    //--------------------------------------------------------------------------
+    // Initialization
+
+    /// @inheritdoc Module
+    function init(
+        IOrchestrator orchestrator_,
+        Metadata memory metadata,
+        bytes memory configData
+    ) external virtual override(Module) initializer {
+        __Module_init(orchestrator_, metadata);
+
+        _setStakingToken(abi.decode(configData, (address)));
+    }
+
+    //--------------------------------------------------------------------------
+    // Getter Functions
+
+    /// @inheritdoc IStakingManager
+    function balanceOf(address user) external view returns (uint) {
+        return _balances[user];
+    }
+
+    /// @inheritdoc IStakingManager
+    function earned(address user) external view returns (uint) {
+        return _earned(user, _calculateRewardValue());
+    }
+
+    /// @inheritdoc IStakingManager
+    function estimateReward(uint amount, uint duration)
+        external
+        view
+        validAmount(amount)
+        validDuration(duration)
+        returns (uint)
+    {
+        //if rewardsend is reached you dont get any rewards
+        if (block.timestamp > rewardsEnd) {
+            return 0;
+        }
+        //If duration went over rewardsend
+        if (block.timestamp + duration > rewardsEnd) {
+            //change duration so that it goes until rewardsend
+            duration = rewardsEnd - block.timestamp;
+        }
+        //If no one else staked
+        if (totalSupply == 0) {
+            //Get full amount back
+            return amount * duration * rewardRate;
+        }
+        return amount * duration * rewardRate / totalSupply;
+    }
+
+    //--------------------------------------------------------------------------
+    // Mutating Functions
+
+    /// @inheritdoc IStakingManager
+    function stake(uint amount)
+        external
+        virtual
+        nonReentrant
+        validAmount(amount)
+    {
+        address sender = _msgSender();
+
+        _stake(sender, amount);
+
+        //transfer funds to stakingManager
+        IERC20(stakingToken).safeTransferFrom(sender, address(this), amount);
+    }
+
+    /// @inheritdoc IStakingManager
+    /// @dev this function will revert with a Over/Underflow error in case amount is higher than balance
+    function unstake(uint amount)
+        external
+        virtual
+        nonReentrant
+        validAmount(amount)
+    {
+        address sender = _msgSender();
+        //Update rewardValue, updatedTimestamp and earned values
+        _update(sender);
+
+        //Reduce balances accordingly
+        _balances[sender] -= amount;
+        //Total supply too
+        totalSupply -= amount;
+
+        //Transfer funds back to sender
+        IERC20(stakingToken).safeTransfer(sender, amount);
+
+        //If the user has earned something
+        if (rewards[sender] != 0) {
+            //distribute rewards
+            _distributeRewards(sender);
+        }
+
+        emit Unstaked(sender, amount);
+    }
+
+    /// @inheritdoc IStakingManager
+    function setRewards(uint amount, uint duration)
+        external
+        onlyOrchestratorOwnerOrManager
+    {
+        _setRewards(amount, duration);
+    }
+
+    //--------------------------------------------------------------------------
+    // Private Functions
+
+    function _stake(address depositFor, uint amount) internal virtual {
+        _update(depositFor);
+
+        //If the user has already earned something
+        if (rewards[depositFor] != 0) {
+            //distribute rewards for previous reward period
+            _distributeRewards(depositFor);
+        }
+
+        //Increase balance accordingly
+        _balances[depositFor] += amount;
+        //Total supply too
+        totalSupply += amount;
+
+        emit Staked(depositFor, amount);
+    }
+
+    /// @dev This has to trigger on every major change of the state of the contract
+    function _update(address triggerAddress) internal {
+        //Set a new reward value
+        rewardValue = _calculateRewardValue();
+
+        //Set timestamp correctly
+        lastUpdate = _getRewardDistributionTimestamp();
+
+        //If trigger address is 0 then its not a user
+        if (triggerAddress != address(0)) {
+            rewards[triggerAddress] = _earned(triggerAddress, rewardValue);
+            userRewardValue[triggerAddress] = rewardValue;
+        }
+    }
+
+    ///@dev This is the heart of the algorithm
+    /// The reward Value is the accumulation of all the rewards a user would get for a single token if they had staked at the beginning of the lifetime of this contract
+    /// A "single" reward value or with the lack of a better word "reward period" is the rewardRate (so the rewards per second for the whole contract)
+    /// multiplied by the time period it was active and dividing that with the total supply
+    /// This "single" value is essentially what a single token would have earned in that time period
+    function _calculateRewardValue() internal view returns (uint) {
+        //In case the totalSupply is 0 the rewardValue doesnt change
+        if (totalSupply == 0) {
+            return rewardValue;
+        }
+
+        return (_getRewardDistributionTimestamp() - lastUpdate) //Get the time difference between the last time it was updated and now (or in case the reward period ended the rewardEnd timestamp)
+            * rewardRate //Multiply it with the rewardrate to get the rewards distributed for all of the stakers together
+            * 1e18 // for the later division we need a value to compensate for the loss of precision. This value will be counteracted in earned()
+            / totalSupply //divide it by the totalSupply to get the rewards per token
+            + rewardValue; //add the old rewardValue to the new "single" rewardValue
+    }
+
+    /// @dev The function returns either the current timestamp or the last timestamp where rewards will be distributed, based on which one is earlier
+    /// Is necessary to calculate the exact rewardValue at the end of the reward lifespan
+    /// If not included rewards would be distributed forever
+    function _getRewardDistributionTimestamp() internal view returns (uint) {
+        return rewardsEnd <= block.timestamp ? rewardsEnd : block.timestamp;
+    }
+
+    /// @dev internal function to calculate how much a user earned for their stake up to this point
+    /// Uses the difference between the current Reward Value and the reward value when the user staked their tokens
+    /// in combination with their current balance to calculate their earnings
+    function _earned(address user, uint providedRewardValue)
+        internal
+        view
+        returns (uint)
+    {
+        return (providedRewardValue - userRewardValue[user]) //This difference in rewardValues basically represents the time period between now and the moment the userRewardValue was created
+            * _balances[user] // multiply by users balance of tokens to get their share of the token rewards
+            / 1e18 // See comment in _calculateRewardValue();
+            + rewards[user];
+    }
+
+    ///@dev direct distribution of earned rewards via the payment processor
+    function _distributeRewards(address recipient) internal {
+        //Check what recipient has earned
+        uint amount = _earned(recipient, rewardValue);
+        //Set rewards to zero
+        rewards[recipient] = 0;
+
+        _addPaymentOrder(
+            PaymentOrder({
+                recipient: recipient,
+                amount: amount,
+                createdAt: block.timestamp,
+                dueTo: block.timestamp
+            })
+        );
+
+        __Module_orchestrator.paymentProcessor().processPayments(
+            IERC20PaymentClient(address(this))
+        );
+
+        emit RewardsDistributed(recipient, amount);
+    }
+
+    /// @dev for contracts that inherit
+    function _setRewards(uint amount, uint duration)
+        internal
+        validAmount(amount)
+        validDuration(duration)
+    {
+        _update(address(0));
+        //If rewardsEnd is already reached
+        if (block.timestamp >= rewardsEnd) {
+            //Dont include previous reward Rate
+            rewardRate = amount / duration;
+        } else {
+            //Calculate remaining rewards supposed to go back into the pool
+            uint remainingRewards = (rewardsEnd - block.timestamp) * rewardRate;
+            //Add new Amount to previous amount and calculate rate
+            rewardRate = (amount + remainingRewards) / duration;
+        }
+
+        //RewardRate cant be zero
+        if (rewardRate == 0) {
+            revert Module__StakingManager__InvalidRewardRate();
+        }
+
+        //Rewards end is now plus duration
+        rewardsEnd = block.timestamp + duration;
+        //Update lastUpdate or calculation of rewards would include timeperiod where no rewards should have been distributed
+        lastUpdate = block.timestamp;
+
+        emit RewardSet(amount, duration, rewardRate, rewardsEnd);
+    }
+
+    function _setStakingToken(address _token) internal {
+        if (_token == address(0)) {
+            revert Module__StakingManager__InvalidStakingToken();
+        }
+        stakingToken = _token;
+    }
+}
