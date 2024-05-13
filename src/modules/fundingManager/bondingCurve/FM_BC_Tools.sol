4b17f10dd26745c9f3dd467955b3764d8086a245
0xNuggan <82726722+0xNuggan@users.noreply.github.com>
2024-05-13 18:03:07 +0200

    remove commented code

--- src/modules/fundingManager/bondingCurve/FM_BC_Tools.sol
+++ src/modules/fundingManager/bondingCurve/FM_BC_Tools.sol
@@ -2,28 +2,8 @@
 pragma solidity 0.8.23;
 
 library FM_BC_Tools {
-    error Module__BondingCurveBase__FeeAmountToHigh();
-    error Module__FM_BC_Bancor_Redeeming_VirtualSupply__InvalidReserveRatio();
-
-    /*     /// @dev Calculates the static price for either selling or buying the issuance token,
-    /// based on the provided issuance token supply, collateral supply, and buy or sell reserve ratio.
-    /// Note: The reserve ratio specifies whether the sell or buy price is returned.
-    /// The formula used is: PPM * PPM * collateralSupply / (issuanceTokenSupply * reserveRatio).
-    /// The formula is based on Aragon's BatchedBancorMarketMaker, which can be found here:
-    /// https://github.com/AragonBlack/fundraising/blob/5ad1332955bab9d36cfad345ae92b7ad7dc0bdbe/apps/batched-bancor-market-maker/contracts/BatchedBancorMarketMaker.sol#L415
-    /// @param _issuanceSupply The total supply of the issuance tokens.
-    /// @param _collateralSupply The total supply of the collateral held by the FundingManager.
-    /// @param _reserveRatio The reserve ratio, specified as either sell or buy reserve ratio.
-    /// @return uint The calculated static price for the specified operation, formatted in PPM
-    function _staticPricePPM(
-        uint32 PPM,
-        uint _issuanceSupply,
-        uint _collateralSupply,
-        uint32 _reserveRatio
-    ) internal pure returns (uint) {
-        return uint(PPM) * uint(PPM) * _collateralSupply
-            / (_issuanceSupply * uint(_reserveRatio));
-    }*/
+    //error Module__BondingCurveBase__FeeAmountToHigh();
+    
 
     /// @dev Converts an amount to a required decimal representation.
     /// This function is useful for handling tokens with different decimal places.
@@ -55,26 +35,7 @@ library FM_BC_Tools {
             return (_amount * conversionFactor);
         }
     }
-    /*
-        /// @dev Calculates the net amount after fee deduction and the fee amount based on
-    /// a transaction amount and a specified fee percentage.
-    /// @param _transactionAmount The amount involved in the transaction before fee deduction.
-    /// @param _feePct The fee percentage to be deducted, represented in basis points (BPS).
-    /// @return netAmount The transaction amount after fee deduction.
-    /// @return feeAmount The amount of fee deducted from the transaction amount.
-    function _calculateNetAmountAndFee(uint _transactionAmount, uint _feePct)
-        internal
-        pure
-        virtual
-        returns (uint netAmount, uint feeAmount)
-    {
-        // Return transaction amount as net amount if fee percentage is zero
-        if (_feePct == 0) return (_transactionAmount, feeAmount);
-        // Calculate fee amount
-        feeAmount = (_transactionAmount * _feePct) / BPS;
-        // Calculate net amount after fee deduction
-        netAmount = _transactionAmount - feeAmount;
-    }*/
+  
     /*
     function _calculateNetAndSplitFees(
         uint _totalAmount,
@@ -82,7 +43,7 @@ library FM_BC_Tools {
         uint _workflowFee,
         uint BPS
     )
-        public
+        external
         pure
         returns (uint netAmount, uint protocolFeeAmount, uint workflowFeeAmount)
     {
