35112f084077bacd94dfeafc0c60bbb97a05521d
0xNuggan <82726722+0xNuggan@users.noreply.github.com>
2024-05-20 11:03:22 +0200

    tests pass, missing cleanup

--- src/modules/logicModule/abstracts/ERC20PaymentClientBase_v1.sol
+++ src/modules/logicModule/abstracts/ERC20PaymentClientBase_v1.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity 0.8.23;
 
+import "forge-std/console.sol";
+
 // External Libraries
 import {SafeERC20} from "@oz/token/ERC20/utils/SafeERC20.sol";
 
--- src/modules/paymentProcessor/PP_Simple_v1.sol
+++ src/modules/paymentProcessor/PP_Simple_v1.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity 0.8.23;
 
+import "forge-std/console.sol";
+
 // Internal Interfaces
 import {IOrchestrator_v1} from
     "src/orchestrator/interfaces/IOrchestrator_v1.sol";
