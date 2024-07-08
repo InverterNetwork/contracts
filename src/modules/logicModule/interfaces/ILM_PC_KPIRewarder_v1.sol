b71e78fb36e822c9a026e01568b3d9247579d43e
0xNuggan <82726722+0xNuggan@users.noreply.github.com>
2024-07-08 17:46:52 +0200

    WIP

--- src/modules/logicModule/interfaces/ILM_PC_KPIRewarder_v1.sol
+++ src/modules/logicModule/interfaces/ILM_PC_KPIRewarder_v1.sol
@@ -59,7 +59,13 @@ interface ILM_PC_KPIRewarder_v1 {
     error Module__LM_PC_KPIRewarder_v1__UnresolvedAssertionExists();
 
     /// @notice Callback received references non existent assertionId
-    error Module__LM_PC_KPIRewarder_v1__CallbackFromNonexistentAssertionId();
+    error Module__LM_PC_KPIRewarder_v1__NonExistentAssertionId(
+        bytes32 assertionId
+    );
+
+    /// @notice The assertion that is being removed was not stuck
+    error Module__LM_PC_KPIRewarder_v1__AssertionNotStuck();
+
     //--------------------------------------------------------------------------
     // Events
 
@@ -88,6 +94,9 @@ interface ILM_PC_KPIRewarder_v1 {
 
     /// @notice Event emitted when funds for paying the bonding fee are deposited into the contract
     event FeeFundsDeposited(address indexed token, uint amount);
+
+    /// @notice Event emitted when a stuck assertion gets deleted
+    event DeletedStuckAssertion(bytes32 indexed assertionId);
 
     //--------------------------------------------------------------------------
     // Functions
