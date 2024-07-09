f1ce556e31d176ac1c1a24d9f2a9974693f19f7d
FHieser <felix_hieser@web.de>
2024-07-09 14:17:21 +0200

    Create and Test Inverter reverter

---
+++ src/external/reverter/InverterReverter_v1.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity 0.8.23;
+
+/**
+ * @title   Inverter Reverter
+ *
+ * @notice  Enables the Inverter beacon structure to return a predefined error message in case a paused contract is called.
+ *
+ * @dev     Reverts all transactions with a predefined error
+ *
+ * @custom:security-contact security@inverter.network
+ *                          In case of any concerns or findings, please refer to our Security Policy
+ *                          at security.inverter.network or email us directly!
+ *
+ * @author  Inverter Network
+ */
+contract InverterReverter_v1 {
+    /// @notice The contract that the transactions was meant to interact with is paused
+    error InverterReverter__ContractPaused();
+
+    fallback() external {
+        revert InverterReverter__ContractPaused();
+    }
+}
