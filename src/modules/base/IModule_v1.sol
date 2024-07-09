61453e63647f1711b8dd5393ec63a9b309d0d41e
Fabian Scherer <fabiansche@gmail.com>
2024-07-09 16:30:21 +0200

    chore: remove `indexed` property from title in ModuleInitialized

--- src/modules/base/IModule_v1.sol
+++ src/modules/base/IModule_v1.sol
@@ -20,7 +20,7 @@ interface IModule_v1 {
     /// @param parentOrchestrator The address of the orchestrator the module is linked to.
     /// @param moduleTitle The title of the module.
     event ModuleInitialized(
-        address indexed parentOrchestrator, string indexed moduleTitle
+        address indexed parentOrchestrator, string moduleTitle
     );
 
     //--------------------------------------------------------------------------
