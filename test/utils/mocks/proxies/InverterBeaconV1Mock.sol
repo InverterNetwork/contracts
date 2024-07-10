26c776ee2c881538dee60f9f2d8d20abb74b7433
FHieser <felix_hieser@web.de>
2024-07-10 10:37:18 +0200

    Adapt tests

--- test/utils/mocks/proxies/InverterBeaconV1Mock.sol
+++ test/utils/mocks/proxies/InverterBeaconV1Mock.sol
@@ -16,6 +16,7 @@ contract InverterBeaconV1Mock is IInverterBeacon_v1, ERC165 {
             || super.supportsInterface(interfaceId);
     }
 
+    address public reverter;
     address public implementation;
 
     bool public emergencyMode;
@@ -47,6 +48,10 @@ contract InverterBeaconV1Mock is IInverterBeacon_v1, ERC165 {
 
     function version() external view returns (uint, uint, uint) {
         return (majorVersion, minorVersion, patchVersion);
+    }
+
+    function getReverterAddress() external view virtual returns (address) {
+        return reverter;
     }
 
     function getImplementationAddress() external view returns (address) {
