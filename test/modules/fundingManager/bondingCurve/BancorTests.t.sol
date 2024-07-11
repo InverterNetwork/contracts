f0f24e6d67a86637c1fb413e5320b8c5a2aad1bf
0xNuggan <82726722+0xNuggan@users.noreply.github.com>
2024-07-11 18:07:51 +0200

    more test

--- test/modules/fundingManager/bondingCurve/BancorTests.t.sol
+++ test/modules/fundingManager/bondingCurve/BancorTests.t.sol
@@ -6,8 +6,6 @@ import "forge-std/Test.sol";
 
 import {BancorFormula} from "@fm/bondingCurve/formulas/BancorFormula.sol";
 
-
-
 contract BancorTests is Test {
     address formula;
     BancorFormula bancorFormula;
@@ -18,18 +16,15 @@ contract BancorTests is Test {
     }
 
     function test_BancorFormula_suppylCalculationPlusBuys() public {
-        
         uint PPM = 1_000_000;
         uint32 RESERVE_RATIO = 199_800; // In PPM
 
-    
         // We take the reserve form the logs for the TEC case and calculate the supply
-        uint INITIAL_RESERVE = 50000000000000000000000;
-        uint INITIAL_SUPPLY = PPM * (INITIAL_RESERVE) / (uint(RESERVE_RATIO) );
+        uint INITIAL_RESERVE = 50_000_000_000_000_000_000_000;
+        uint INITIAL_SUPPLY = PPM * (INITIAL_RESERVE) / (uint(RESERVE_RATIO));
 
         console.log("Reserve calculation: \t\t", INITIAL_RESERVE);
         console.log("Supply calculation: \t\t", INITIAL_SUPPLY);
-
 
         // perform a buy that should bring it up to the initial TEC state
         uint amountToBuy = 200_000e18;
@@ -45,19 +40,144 @@ contract BancorTests is Test {
         console.log(" Total Reserve: \t", INITIAL_RESERVE + amountToBuy);
 
         // check
+    }
 
+    uint[] reserveAmounts;
+
+    function test_BancorFormula_SellAndBuyStepByStep() public {
+        uint PPM = 1_000_000;
+        uint32 RESERVE_RATIO = 199_800; // In PPM
+
+        // TEC numbers
+        uint INITIAL_SUPPLY = 195_642_169e16;
+        uint INITIAL_RESERVE = 39_097_931e16;
+
+        // These are minimal numbers. Spot price is 0
+        //uint INITIAL_SUPPLY = 56421690000000000000000;
+        //uint INITIAL_RESERVE = 7662387184363186;
+
+        // THESE ARE THE MINIMAL NUMBERS FOR THE TEC CONFIGURATION: spot price is 1, and a 1e6 buy pushes it to 0.
+        // Please note that large sells can move the price below this amount. But they will need an equally big buys afterwards that pushes the balances over thsi limit again.
+        //uint INITIAL_SUPPLY = 62131676819845007200000;
+        //uint INITIAL_RESERVE = 12413909028605033;
+
+        //Test
+        RESERVE_RATIO = 333_333;
+        INITIAL_SUPPLY = 3_000_003e18;
+        INITIAL_RESERVE = 1_000_000e18;
+
+        uint supply = INITIAL_SUPPLY;
+        uint reserve = INITIAL_RESERVE;
+
+        uint saleStepSize = 100_000e18;
+
+        uint NUM_OF_STEPS = 0;
+        //uint MAX_STEPS = 98;
+        //uint[] storage reserveAmounts;
+        uint curentSpotPrice = 1e12 * reserve / (supply * uint(RESERVE_RATIO));
+
+        /*uint 1_000_000 = 1e12/RESERVE_RATIO * 1_000_000e18/x;
+        x = 1e12/RESERVE_RATIO * 1_000_000e18/1_000_000;
+        x = 1e12/RESERVE_RATIO * 1e18;
+        x = 300000_300000000000000000*/
+
+        // TODO: calculate the way to get the balance at a given reserve point if we have a defined curve
+
+        console.log("Initial spot price: %s", curentSpotPrice);
+
+        while (curentSpotPrice > 0 && NUM_OF_STEPS < 29) {
+            //perform a sale
+
+            uint receivedAmount = bancorFormula.calculateSaleReturn(
+                supply, reserve, RESERVE_RATIO, saleStepSize
+            );
+
+            {
+                //Logs
+                console.log("=================================");
+                console.log("SALE: STEP %s: ", NUM_OF_STEPS);
+                console.log("=================================");
+
+                console.log("\tSupply: \t\t", supply);
+                console.log("\tReserve: \t\t ", reserve);
+                console.log("\n");
+                console.log("Spot Price: \t\t", curentSpotPrice);
+
+                console.log("\tSell Amount: \t", saleStepSize);
+
+                console.log("Received Amount after  sale: ");
+                console.log("\t\t\t\t", receivedAmount);
+                console.log("---------------------------------");
+            }
+            //update state
+            NUM_OF_STEPS++;
+            reserveAmounts.push(receivedAmount);
+            reserve -= receivedAmount;
+            supply -= saleStepSize;
+            curentSpotPrice = 1e12 * reserve / (supply * uint(RESERVE_RATIO));
+        }
+
+        console.log("=================================");
+        console.log("Supply after all sales: \t\t", supply);
+        console.log("Reserve after all sales: \t\t ", reserve);
+        uint spotPriceAfterSales =
+            1e12 * reserve / (supply * uint(RESERVE_RATIO));
+        console.log("Spot Price after all sales: \t\t", spotPriceAfterSales);
+        console.log("=================================");
+
+        for (uint i = 0; i < NUM_OF_STEPS; i++) {
+            //buys
+            uint buyAmount = reserveAmounts[reserveAmounts.length - i - 1];
+            //uint buyAmount = 1e18;
+
+            uint receivedAmount = bancorFormula.calculatePurchaseReturn(
+                supply, reserve, RESERVE_RATIO, buyAmount
+            );
+
+            //if ((i + 1) == NUM_OF_STEPS) {
+            //Logs
+            console.log("=================================");
+            console.log("BUY: STEP %s: ", i + 1);
+            console.log("=================================");
+            console.log("\tSupply: \t\t", supply);
+            console.log("\tReserve: \t\t ", reserve);
+            console.log("\n");
+            uint spotPrice = 1e12 * reserve / (supply * uint(RESERVE_RATIO));
+            console.log("Spot Price: \t\t", spotPrice);
+
+            console.log("\tBuy Amount: \t", buyAmount);
+
+            console.log("Received Amount after a deposit: ");
+            console.log("\t\t\t\t", receivedAmount);
+            console.log("---------------------------------");
+            //}
+
+            reserve += buyAmount;
+            supply += receivedAmount;
+
+            //assertApproxEqAbs(receivedAmount, saleStepSize, 1_000_000);
+        }
+
+        console.log("=================================");
+        console.log("Supply after all buys: \t\t", supply);
+        console.log("Reserve after all buys: \t\t ", reserve);
+        uint spotPriceAfterBuys =
+            1e12 * reserve / (supply * uint(RESERVE_RATIO));
+        console.log("Spot Price after all buys: \t\t", spotPriceAfterBuys);
+        console.log("=================================");
     }
 
     function test_BancorFormula_LowBalancesThroughSale() public {
-           uint PPM = 1_000_000;
+        uint PPM = 1_000_000;
         uint32 RESERVE_RATIO = 199_800; // In PPM
 
-        uint INITIAL_SUPPLY = 195642169e16;
-        uint INITIAL_RESERVE = 39097931e16;
+        uint INITIAL_SUPPLY = 195_642_169e16;
+        uint INITIAL_RESERVE = 39_097_931e16;
 
-        uint fixedStaticPrice = 1000220;
+        uint fixedStaticPrice = 1_000_220;
 
-        uint supplyCalculation = PPM* PPM * (66168439387169789) / (uint(RESERVE_RATIO) * 3);
+        uint supplyCalculation =
+            PPM * PPM * (66_168_439_387_169_789) / (uint(RESERVE_RATIO) * 3);
 
         console.log("Supply calculation: \t\t", supplyCalculation);
 
@@ -67,35 +187,32 @@ contract BancorTests is Test {
         uint supply = INITIAL_SUPPLY;
         uint reserve = INITIAL_RESERVE;
 
-        uint sellAmount = 1869622396000000000000000;
+        uint sellAmount = 1_869_622_396_000_000_000_000_000;
 
-                console.log("=================================");
-            console.log("TEST SALE: ");
-            console.log("=================================");
+        console.log("=================================");
+        console.log("TEST SALE: ");
+        console.log("=================================");
 
-            console.log("\tSupply: \t\t", supply);
-            console.log("\tReserve: \t\t ", reserve);
-            console.log("\n");
-            uint spotPrice = 1e12 * reserve
-            / (supply * uint(RESERVE_RATIO));
-            console.log("Spot Price: \t\t", spotPrice);
-            console.log("\tSell Amount: \t", sellAmount);
-            uint receivedAmount = bancorFormula.calculateSaleReturn(
-                supply, reserve, RESERVE_RATIO, sellAmount
-            );
-            console.log("Received Amount after a sale: ");
-            console.log("\t\t\t\t", receivedAmount);
+        console.log("\tSupply: \t\t", supply);
+        console.log("\tReserve: \t\t ", reserve);
+        console.log("\n");
+        uint spotPrice = 1e12 * reserve / (supply * uint(RESERVE_RATIO));
+        console.log("Spot Price: \t\t", spotPrice);
+        console.log("\tSell Amount: \t", sellAmount);
+        uint receivedAmount = bancorFormula.calculateSaleReturn(
+            supply, reserve, RESERVE_RATIO, sellAmount
+        );
+        console.log("Received Amount after a sale: ");
+        console.log("\t\t\t\t", receivedAmount);
 
-            console.log(" Remaining Supply: \t", supply - sellAmount);
-            console.log(" Remaining Reserve: \t", reserve - receivedAmount);
+        console.log(" Remaining Supply: \t", supply - sellAmount);
+        console.log(" Remaining Reserve: \t", reserve - receivedAmount);
 
-            uint newSpotPrice = 1e12 * (reserve - receivedAmount)
+        uint newSpotPrice = 1e12 * (reserve - receivedAmount)
             / ((supply - sellAmount) * uint(RESERVE_RATIO));
 
-            console.log("New Spot Price: \t\t", newSpotPrice);
+        console.log("New Spot Price: \t\t", newSpotPrice);
     }
-
-
 
     function test_BancorFormula_StepByStep() public {
         uint PPM = 1_000_000;
@@ -103,26 +220,24 @@ contract BancorTests is Test {
 
         //uint INITIAL_SUPPLY = 195642169e16;
         //uint INITIAL_RESERVE = 39097931e16;
-
-        uint INITIAL_SUPPLY = 1e12;
-        uint INITIAL_RESERVE = 199_800;
+        uint32 RESERVE_RATIO = 199_800; // In PPM
+        uint INITIAL_SUPPLY = 200_002_999_999_999_999_998_676;
+        uint INITIAL_RESERVE = 296_306_333_665_498_798_599;
 
         uint supply = INITIAL_SUPPLY;
         uint reserve = INITIAL_RESERVE;
 
-        uint buyPerStep = 39097930999999999800200;
+        uint buyPerStep = 10e18;
 
-       for (uint i = 0; i < 10; i++) {
-                    console.log("=================================");
+        for (uint i = 0; i < 10; i++) {
+            console.log("=================================");
             console.log("STEP %s: ", i + 1);
             console.log("=================================");
 
             console.log("\tSupply: \t\t", supply);
             console.log("\tReserve: \t\t ", reserve);
             console.log("\n");
-            uint spotPrice = 1e12 * reserve
-            / (supply * uint(RESERVE_RATIO));
-
+            uint spotPrice = 1e12 * reserve / (supply * uint(RESERVE_RATIO));
 
             console.log("Spot Price: \t\t", spotPrice);
             console.log("\tDeposit Amount: \t", buyPerStep);
@@ -137,10 +252,8 @@ contract BancorTests is Test {
         }
     }
 
-
     function test_BancorFormula_WithExplanations() public {
         // VARIABLES
-
 
         uint PPM = 1_000_000;
 
@@ -184,13 +297,13 @@ contract BancorTests is Test {
             console.log("\n");
             console.log("\tDeposit Amount: \t", depositAmount);
 
-            uint spotPriceBuy = (BALANCE_BASE )
-            / ((SUPPLY_BASE) * uint(RESERVE_RATIO));
+            uint spotPriceBuy =
+                (BALANCE_BASE) / ((SUPPLY_BASE) * uint(RESERVE_RATIO));
 
             console.log("Spot Price for buying: \t\t", spotPriceBuy);
 
-            uint spotPriceSell = (SUPPLY_BASE )
-            / ((BALANCE_BASE) * uint(RESERVE_RATIO));
+            uint spotPriceSell =
+                (SUPPLY_BASE) / ((BALANCE_BASE) * uint(RESERVE_RATIO));
 
             console.log("Spot Price for selling: \t\t", spotPriceSell);
 
@@ -202,7 +315,7 @@ contract BancorTests is Test {
             console.log("\t\t\t\t", receivedAmount);
             console.log("---------------------------------");
             console.log("\n");
-/*
+            /*
             console.log("With \"normal\" supply numbers:");
             console.log("---------------------------------");
             console.log("\tSupply: \t\t", SUPPLY_BASE * 1e18);
