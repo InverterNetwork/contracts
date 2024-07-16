dde65924f5cde12800c9c96575c4231c49912f98
Fabian Scherer <fabiansche@gmail.com>
2024-07-16 12:12:32 +0200

    chore: OrchestratorFactory_v1 (adds init event)

--- src/factories/OrchestratorFactory_v1.sol
+++ src/factories/OrchestratorFactory_v1.sol
@@ -133,9 +133,9 @@ contract OrchestratorFactory_v1 is
         ) {
             revert OrchestratorFactory__InvalidBeacon();
         }
-
         beacon = beacon_;
         moduleFactory = moduleFactory_;
+        emit OrchestratorFactoryInitialized(address(beacon_), moduleFactory_);
     }
 
     //--------------------------------------------------------------------------
