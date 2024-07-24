d55b9ed017660d6212324ce17ea97e16d337020f
FHieser <felix_hieser@web.de>
2024-07-24 13:59:24 +0200

    Update Module

--- src/modules/base/Module_v1.sol
+++ src/modules/base/Module_v1.sol
@@ -50,6 +50,7 @@ abstract contract Module_v1 is
     ERC2771ContextUpgradeable,
     ERC165Upgradeable
 {
+    /// @inheritdoc ERC165
     function supportsInterface(bytes4 interfaceId)
         public
         view
@@ -113,6 +114,7 @@ abstract contract Module_v1 is
         _;
     }
 
+    /// @notice Modifier to guarantee function is only callable by addresses that hold a specific module-assigned role.
     modifier onlyModuleRoleAdmin(bytes32 role) {
         bytes32 moduleRole = __Module_orchestrator.authorizer().generateRoleId(
             address(this), role
@@ -206,6 +208,7 @@ abstract contract Module_v1 is
     //--------------------------------------------------------------------------
     // Role Management
 
+    /// @inheritdoc IModule_v1
     function grantModuleRole(bytes32 role, address target)
         external
         onlyModuleRoleAdmin(role)
@@ -213,6 +216,7 @@ abstract contract Module_v1 is
         __Module_orchestrator.authorizer().grantRoleFromModule(role, target);
     }
 
+    /// @inheritdoc IModule_v1
     function grantModuleRoleBatched(bytes32 role, address[] calldata targets)
         external
         onlyModuleRoleAdmin(role)
@@ -222,6 +226,7 @@ abstract contract Module_v1 is
         );
     }
 
+    /// @inheritdoc IModule_v1
     function revokeModuleRole(bytes32 role, address target)
         external
         onlyModuleRoleAdmin(role)
@@ -229,6 +234,7 @@ abstract contract Module_v1 is
         __Module_orchestrator.authorizer().revokeRoleFromModule(role, target);
     }
 
+    /// @inheritdoc IModule_v1
     function revokeModuleRoleBatched(bytes32 role, address[] calldata targets)
         external
         onlyModuleRoleAdmin(role)
@@ -279,12 +285,33 @@ abstract contract Module_v1 is
         );
     }
 
+    /// @dev Internal function to trigger a callback from the orchestrator.
+    /// @param data The call data for the orchestrator to call.
+    /// @return Whether the callback succeeded.
+    /// @return The return data of the callback.
+    function _triggerOrchestratorCallback(bytes memory data)
+        internal
+        returns (bool, bytes memory)
+    {
+        // Note that there is no check whether the orchestrator callback succeeded.
+        // This responsibility is delegated to the caller, i.e. downstream
+        // module implementation.
+        // However, the {IModule_v1} interface defines a generic error type for
+        // failed orchestrator callbacks that can be used to prevent different
+        // custom error types in each implementation.
+        return __Module_orchestrator.executeTxFromModule(address(this), data);
+    }
+
+    /// @dev Checks if the caller has the specified role.
+    /// @param role The role to check.
+    /// @param addr The address to check.
     function _checkRoleModifier(bytes32 role, address addr) internal view {
         if (!__Module_orchestrator.authorizer().hasRole(role, addr)) {
             revert Module__CallerNotAuthorized(role, addr);
         }
     }
 
+    /// @dev Checks if the caller is the orchestrator.
     function _onlyOrchestratorModifier() internal view {
         if (_msgSender() != address(__Module_orchestrator)) {
             revert Module__OnlyCallableByOrchestrator();
