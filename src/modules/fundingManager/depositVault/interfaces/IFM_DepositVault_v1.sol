916bff87501b2a2a46055d9aaaff524b73cd8da6
Zitzak <marvin88gr@gmail.com>
2024-07-29 13:02:30 +0200

    feat: add depositFor function to FM_DepositVault

--- src/modules/fundingManager/depositVault/interfaces/IFM_DepositVault_v1.sol
+++ src/modules/fundingManager/depositVault/interfaces/IFM_DepositVault_v1.sol
@@ -18,4 +18,13 @@ interface IFM_DepositVault_v1 {
     /// @dev When using the transactionForwarder, validate transaction success to prevent nonce exploitation and ensure transaction integrity.
     /// @param amount The number of tokens to deposit.
     function deposit(uint amount) external;
+
+    /// @notice Deposits a specified amount of tokens into the contract for the address defined in
+    ///         the `from` parameter.
+    /// @dev    Reverts if attempting self-deposits
+    /// @dev    Note: when using the transactionForwarder,
+    ///         validate transaction success to prevent nonce exploitation and ensure transaction integrity.
+    /// @param from The address to deposit tokens for.
+    /// @param amount The number of tokens to deposit.
+    function depositFor(address from, uint amount) external;
 }
