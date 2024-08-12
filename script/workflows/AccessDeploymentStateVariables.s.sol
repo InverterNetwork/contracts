55274f0c7b4c0d5954ce8c2c9f5b78e41d424a67
0xNuggan <82726722+0xNuggan@users.noreply.github.com>
2024-08-12 17:35:41 +0200

    rename accesDeploymentState and celan Test mention

--- script/workflows/AccessDeploymentStateVariables.s.sol
+++ script/workflows/AccessDeploymentStateVariables.s.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "forge-std/Test.sol";
-
 import {TestnetDeploymentScript} from
     "script/deploymentScript/TestnetDeploymentScript.s.sol";
 
