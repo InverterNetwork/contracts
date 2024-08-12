48b66d0a1452d17da64e563e4a9fb44f3f6ae430
Marvin Kruse <marvinkruse@users.noreply.github.com>
2024-08-12 14:05:54 +0200

    Scripts: Improve Scripts Before Deployment (#649)
    * Fix: Set dependecy version to v1.0 tag
    
    * Script: Improve scripts, add checks & clean logs
    
    - Harmonize the logged output so it looks good
    - Add checks in scripts to ensure that deployment was successful
    - Ensure correctly set addresses, otherwise revert via check modifier
    - On testnet, use deployer for pre-set addresses
    - Simplify env file
    
    Co-authored-by: 0xNuggan <82726722+0xNuggan@users.noreply.github.com>
    
    * Feat: Deploy BancorFormula always as library
    
    * Feat: Check factory before deploying
    
    ---------
    
    Co-authored-by: 0xNuggan <82726722+0xNuggan@users.noreply.github.com>

--- script/deploymentScript/DeploymentScript.s.sol
+++ script/deploymentScript/DeploymentScript.s.sol
@@ -1,15 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "forge-std/Test.sol";
+import "forge-std/Script.sol";
 
+// Scripts
 import {ModuleBeaconDeployer_v1} from
     "script/deploymentSuite/ModuleBeaconDeployer_v1.s.sol";
 
+// Constants
 import {Governor_v1} from "@ex/governance/Governor_v1.sol";
 import {FeeManager_v1} from "@ex/fees/FeeManager_v1.sol";
 import {ModuleFactory_v1} from "src/factories/ModuleFactory_v1.sol";
 import {OrchestratorFactory_v1} from "src/factories/OrchestratorFactory_v1.sol";
+import {IDeterministicFactory_v1} from
+    "@df/interfaces/IDeterministicFactory_v1.sol";
 
 /**
  * @title Inverter Deployment Script
@@ -31,27 +35,37 @@ contract DeploymentScript is ModuleBeaconDeployer_v1 {
     address public moduleFactory;
     address public orchestratorFactory;
 
-    function run() public virtual {
+    function run()
+        public
+        virtual
+        verifyRequiredParameters
+        verifyDeterministicFactory
+    {
+        console2.log();
         console2.log(
-            "--------------------------------------------------------------------------------"
+            "================================================================================"
         );
-        console2.log("Start Deployment Script");
+        console2.log("Start Core Protocol Deployment Script");
+        console2.log(
+            "================================================================================"
+        );
+
+        logProtocolMultisigsAndAddresses();
 
         // Create External Singletons
         createExternalSingletons();
 
-        console2.log(
-            "Set InverterReverter Implementation Address to general InverterReverter Address"
-        );
+        // Create Library Singletons
+        createLibrarySingletons();
 
-        // Set InverterReverter Address
+        // Set InverterReverter Implementation Address to general InverterReverter Address
         inverterReverter = impl_ext_InverterReverter_v1;
 
         // Deploy External Contracts
         console2.log(
             "--------------------------------------------------------------------------------"
         );
-        console2.log("Deploy External Contracts");
+        console2.log(" Deploy External Contracts");
 
         governor = proxyAndBeaconDeployer.deployBeaconAndSetupProxy(
             governorMetadata.title,
@@ -90,7 +104,7 @@ contract DeploymentScript is ModuleBeaconDeployer_v1 {
         console2.log(
             "--------------------------------------------------------------------------------"
         );
-        console2.log("Deploy Factory Contracts");
+        console2.log(" Deploy Factory Contracts");
 
         moduleFactory = proxyAndBeaconDeployer.deployBeaconAndSetupProxy(
             moduleFactoryMetadata.title,
@@ -116,31 +130,133 @@ contract DeploymentScript is ModuleBeaconDeployer_v1 {
         deployModuleBeaconsAndFillRegistrationData(inverterReverter, governor);
 
         // Initialize Protocol Contracts
+        console2.log();
         console2.log(
             "--------------------------------------------------------------------------------"
         );
         console2.log("Initialize Protocol Contracts");
 
+        logProtocolConfigurationData();
+
+        console2.log(
+            "--------------------------------------------------------------------------------"
+        );
+        vm.startBroadcast(deployerPrivateKey);
+        {
+            console2.log(" Initializing...");
+
+            // Governor
+            Governor_v1(governor).init(
+                communityMultisig,
+                teamMultisig,
+                governor_timelockPeriod,
+                feeManager,
+                moduleFactory
+            );
+            console2.log("\t... Governor initialized");
+
+            // FeeManager
+            FeeManager_v1(feeManager).init(
+                governor,
+                treasury,
+                feeManager_defaultCollateralFee,
+                feeManager_defaultIssuanceFee
+            );
+            console2.log("\t... FeeManager initialized");
+
+            // ModuleFactory
+            ModuleFactory_v1(moduleFactory).init(
+                governor, initialMetadataRegistration, initialBeaconRegistration
+            );
+            console2.log("\t... ModuleFactory initialized");
+
+            // OrchestratorFactory
+            OrchestratorFactory_v1(orchestratorFactory).init(
+                governor, orchestratorBeacon, moduleFactory
+            );
+            console2.log("\t... OrchestratorFactory initialized");
+            console2.log(
+                "--------------------------------------------------------------------------------"
+            );
+        }
+        vm.stopBroadcast();
+
+        // ------------------------------------------------------------------------
+        // In order to verify that the deployment was successful, we
+        // verify that the core contracts have been initialized correctly.
+
         // Governor
-        console2.log("-Governor");
-        Governor_v1(governor).init(
-            communityMultisig, teamMultisig, 1 weeks, feeManager, moduleFactory
+        require(
+            Governor_v1(governor).hasRole(
+                Governor_v1(governor).COMMUNITY_MULTISIG_ROLE(),
+                communityMultisig
+            ) == true,
+            "Deployment failed - Governor not initialized correctly, Community Multisig is not set."
+        );
+        require(
+            Governor_v1(governor).hasRole(
+                Governor_v1(governor).TEAM_MULTISIG_ROLE(), teamMultisig
+            ) == true,
+            "Deployment failed - Governor not initialized correctly, Team Multisig is not set."
+        );
+        require(
+            Governor_v1(governor).getModuleFactory() == moduleFactory,
+            "Deployment failed - Governor not initialized correctly, ModuleFactory is not set."
+        );
+        require(
+            Governor_v1(governor).getFeeManager() == feeManager,
+            "Deployment failed - Governor not initialized correctly, FeeManager is not set."
         );
 
-        // FeeManager
-        console2.log("-FeeManager");
-        FeeManager_v1(feeManager).init(governor, treasury, 100, 100);
-
         // ModuleFactory
-        console2.log("-ModuleFactory");
-        ModuleFactory_v1(moduleFactory).init(
-            governor, initialMetadataRegistration, initialBeaconRegistration
+        require(
+            ModuleFactory_v1(moduleFactory).owner() == governor,
+            "Deployment failed - ModuleFactory not initialized correctly, not owned by Governor."
         );
 
         // OrchestratorFactory
-        console2.log("-OrchestratorFactory");
-        OrchestratorFactory_v1(orchestratorFactory).init(
-            governor, orchestratorBeacon, moduleFactory
+        require(
+            OrchestratorFactory_v1(orchestratorFactory).moduleFactory()
+                == moduleFactory,
+            "Deployment failed - OrchestratorFactory not initialized correctly, ModuleFactory is not set."
         );
+
+        // FeeManager
+        require(
+            FeeManager_v1(feeManager).owner() == governor,
+            "Deployment failed - FeeManager not initialized correctly, not owned by Governor."
+        );
+
+        // ------------------------------------------------------------------------
+    }
+
+    modifier verifyRequiredParameters() {
+        require(
+            communityMultisig != address(0),
+            "Community Multisig address not set - aborting!"
+        );
+        require(
+            teamMultisig != address(0),
+            "Team Multisig address not set - aborting!"
+        );
+        require(treasury != address(0), "Treasury address not set - aborting!");
+        require(
+            deterministicFactory != address(0),
+            "Deterministic Factory address not set correctly - aborting!"
+        );
+        _;
+    }
+
+    modifier verifyDeterministicFactory() {
+        require(
+            deterministicFactory.code.length != 0,
+            "Deterministic Factory does not exist at given address - aborting!"
+        );
+        require(
+            IDeterministicFactory_v1(deterministicFactory).allowedDeployer()
+                == deployer,
+            "Deterministic Factory hasn't allowed the deployer - aborting!"
+        );
+        _;
     }
 }
