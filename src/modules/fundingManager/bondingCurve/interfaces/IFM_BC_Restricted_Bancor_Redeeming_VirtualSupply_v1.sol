538de87defa9be927f221cc5e5af7738c4090a17
Fabian Scherer <fabiansche@gmail.com>
2024-08-31 01:35:05 +0200

    chore: fmt

--- src/modules/fundingManager/bondingCurve/interfaces/IFM_BC_Restricted_Bancor_Redeeming_VirtualSupply_v1.sol
+++ src/modules/fundingManager/bondingCurve/interfaces/IFM_BC_Restricted_Bancor_Redeeming_VirtualSupply_v1.sol
@@ -4,11 +4,10 @@ pragma solidity ^0.8.0;
 interface IFM_BC_Restricted_Bancor_Redeeming_VirtualSupply_v1 {
     //--------------------------------------------------------------------------
     // Errors
-    
+
     /// @notice The feature is deactivated in this implementation.
     error Module__FM_BC_Restricted_Bancor_Redeeming_VirtualSupply__FeatureDeactivated(
     );
-
 
     //--------------------------------------------------------------------------
     // Functions
