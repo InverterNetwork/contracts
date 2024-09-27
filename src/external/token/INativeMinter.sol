5cb96fec111d77bae02a56431078935fe49e41ea
Doga Oztuzun <unquale@gmail.com>
2024-09-27 17:23:51 +0000

    fix forge formatter

--- src/external/token/INativeMinter.sol
+++ src/external/token/INativeMinter.sol
@@ -1,9 +1,13 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 import "./IAllowList.sol";
 
 interface INativeMinter is IAllowList {
-  event NativeCoinMinted(address indexed sender, address indexed recipient, uint256 amount);
-  // Mint [amount] number of native coins and send to [addr]
-  function mintNativeCoin(address addr, uint256 amount) external;
+    event NativeCoinMinted(
+        address indexed sender, address indexed recipient, uint amount
+    );
+    // Mint [amount] number of native coins and send to [addr]
+
+    function mintNativeCoin(address addr, uint amount) external;
 }
--- test/utils/mocks/external/NativeMinterMock.sol
+++ test/utils/mocks/external/NativeMinterMock.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
+
 import "forge-std/console.sol";
 
 // import {INativeMinter} from "@avalabs/subnet-evm-contracts@1.2.0/contracts/interfaces/INativeMinter.sol";
 
 contract NativeMinterMock {
-    function mintNativeCoin(address to, uint256 amount) external {
-        // log balanche of this contract 
+    function mintNativeCoin(address to, uint amount) external {
+        // log balanche of this contract
         console.log("NativeMinterMock: balance: %d", address(this).balance);
 
-        (bool success, ) = payable(address(to)).call{value: amount}("");
-        
+        (bool success,) = payable(address(to)).call{value: amount}("");
+
         if (!success) {
             revert("NativeMinterMock: Failed to transfer native token");
         }
