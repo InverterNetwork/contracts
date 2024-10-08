8908e0be24ffc101685a1fb1e73bf06fe9ddb42d
Marvin Kruse <marvinkruse@users.noreply.github.com>
2024-10-08 16:53:19 +0200

    Chore: Fix NatSpec of external interface

--- src/external/interfaces/IOwnable.sol
+++ src/external/interfaces/IOwnable.sol
@@ -2,24 +2,22 @@
 pragma solidity ^0.8.0;
 
 /**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
+ * @title   Ownable
+ * 
+ * @dev     The Ownable contract has an owner address, and provides basic authorization 
+ *          control functions, this simplifies the implementation of "user permissions".
+ *
+ * @author  OpenZeppelin
  */
 interface IOwnable {
-    /**
-     * @dev Returns owner
-     */
+    /// @notice The Owner of the contract.
+    /// @return ownerAddress The address of the owner.
     function owner() external view returns (address ownerAddress);
 
-    /**
-     * @dev Allows the current owner to relinquish control of the contract.
-     */
+    /// @notice Allows the current owner to renounce control of the contract.
     function renounceOwnership() external;
 
-    /**
-     * @dev Allows the current owner to transfer control of the contract to a newOwner.
-     * @param _newOwner The address to transfer ownership to.
-     */
+    /// @notice Allows the current owner to transfer control of the contract to a newOwner.
+    /// @param  _newOwner The address to transfer ownership to.
     function transferOwnership(address _newOwner) external;
 }
