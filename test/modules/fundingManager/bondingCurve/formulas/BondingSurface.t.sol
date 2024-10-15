f75b140198aac857f2f2198f6e1d2a59bd60b1e5
Zitzak <marvin88gr@gmail.com>
2024-10-15 11:21:41 +0200

    Add empty BondingSurface test file

---
+++ test/modules/fundingManager/bondingCurve/formulas/BondingSurface.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+import "forge-std/console.sol";
+import "forge-std/Test.sol";
+
+import {IBondingSurface} from
+    "@fm/bondingCurve/interfaces/IBondingSurface.sol";
+
+import {BondingSurface} from
+    "@fm/bondingCurve/formulas/BondingSurface.sol";
+
+contract BondingSurfaceTest is Test {
+
+
+    
+}
