995aa245549f69b7eb06908f24a62e4ee75fcbd6
FHieser <felix_hieser@web.de>
2024-10-17 09:57:28 +0200

    Update fmt

--- test/modules/fundingManager/bondingCurve/formulas/BondingSurface.t.sol
+++ test/modules/fundingManager/bondingCurve/formulas/BondingSurface.t.sol
@@ -4,14 +4,8 @@ pragma solidity ^0.8.0;
 import "forge-std/console.sol";
 import "forge-std/Test.sol";
 
-import {IBondingSurface} from
-    "@fm/bondingCurve/interfaces/IBondingSurface.sol";
+import {IBondingSurface} from "@fm/bondingCurve/interfaces/IBondingSurface.sol";
 
-import {BondingSurface} from
-    "@fm/bondingCurve/formulas/BondingSurface.sol";
+import {BondingSurface} from "@fm/bondingCurve/formulas/BondingSurface.sol";
 
-contract BondingSurfaceTest is Test {
-
-
-    
-}
+contract BondingSurfaceTest is Test {}
