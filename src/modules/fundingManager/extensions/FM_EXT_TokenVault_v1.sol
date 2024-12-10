3db3fd92a683bd7d25b5471d1b25659afaae323f
FHieser <felix_hieser@web.de>
2024-12-10 10:50:39 +0100

    update modifier Naming

--- src/modules/fundingManager/extensions/FM_EXT_TokenVault_v1.sol
+++ src/modules/fundingManager/extensions/FM_EXT_TokenVault_v1.sol
@@ -49,8 +49,8 @@ contract FM_EXT_TokenVault_v1 is IFM_EXT_TokenVault_v1, Module_v1 {
     // Modifiers
 
     /// @dev    Modifier to guarantee the amount is valid.
-    modifier validAmount(uint amount_) {
-        _validAmount(amount_);
+    modifier amountIsValid(uint amount_) {
+        _amountIsValid(amount_);
         _;
     }
 
@@ -63,7 +63,7 @@ contract FM_EXT_TokenVault_v1 is IFM_EXT_TokenVault_v1, Module_v1 {
         virtual
         onlyOrchestratorAdmin
         validAddress(token_)
-        validAmount(amount_)
+        amountIsValid(amount_)
         validAddress(recipient_)
     {
         IERC20(token_).safeTransfer(recipient_, amount_);
@@ -76,7 +76,7 @@ contract FM_EXT_TokenVault_v1 is IFM_EXT_TokenVault_v1, Module_v1 {
     /// @notice Ensure that the amount is valid.
     /// @dev    Reverts if the amount is invalid, i.e. zero.
     /// @param  amount_ The amount to validate.
-    function _validAmount(uint amount_) internal pure {
+    function _amountIsValid(uint amount_) internal pure {
         if (amount_ == 0) {
             revert Module__FM_EXT_TokenVault__InvalidAmount();
         }
