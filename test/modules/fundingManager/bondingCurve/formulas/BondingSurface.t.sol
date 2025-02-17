342734423d2b904489d9d109e8e863efb2811d1f
0xNuggan <82726722+0xNuggan@users.noreply.github.com>
2025-02-17 18:24:08 +0100

    Fix missing tests

--- test/modules/fundingManager/bondingCurve/formulas/BondingSurface.t.sol
+++ test/modules/fundingManager/bondingCurve/formulas/BondingSurface.t.sol
@@ -10,13 +10,21 @@ import {BondingSurface} from
     "src/modules/fundingManager/bondingCurve/formulas/BondingSurface.sol";
 import {FixedPointMathLib} from "src/modules/lib/FixedPointMathLib.sol";
 
+/* 
+    @note: The comparison values in these tests have been taken from the jupyter 
+        notebooks used for internal research, and are to surface possible imprecisions 
+        in the unit conversion process. 
+        They should not be taken as extensive testing of the formula itself.
+*/
+
 contract BondingSurface_Test is Test {
     using FixedPointMathLib for uint;
 
     BondingSurface public bondingSurface;
     uint constant BASE_MULTIPLIER = 1e11; // 10^-7 * 1e18
     uint constant WAD = 1e18;
-    uint constant REL_TOLERANCE = 1e12; // 0.0001% relative tolerance for complex calculations
+    // 0.0001% relative tolerance for complex calculations
+    uint constant REL_TOLERANCE = 1e12;
 
     function setUp() public {
         bondingSurface = new BondingSurface();
@@ -61,22 +69,22 @@ contract BondingSurface_Test is Test {
         // Test case 1: Ca = 10000.0, in = 1.0, Cr = 50.0
         uint BCr = BASE_MULTIPLIER.fdiv(50 * WAD, WAD); // B/Cr
         uint tokensOut = bondingSurface.tokenOut(1 * WAD, 10_000 * WAD, BCr);
-        assertApproxEqRel(tokensOut, 4.9995e18, REL_TOLERANCE); // 4.9995 * 1e18
+        assertApproxEqRel(tokensOut, 4.9995e18, REL_TOLERANCE);
 
         // Test case 2: Ca = 50.0, in = 1.0, Cr = 25.0
         BCr = BASE_MULTIPLIER.fdiv(25 * WAD, WAD);
         tokensOut = bondingSurface.tokenOut(1 * WAD, 50 * WAD, BCr);
-        assertApproxEqRel(tokensOut, 98_039.22e18, REL_TOLERANCE); // 98039.22 * 1e18
+        assertApproxEqRel(tokensOut, 98_039.22e18, REL_TOLERANCE);
 
         // Test case 3: Ca = 1000000.0, in = 10.0, Cr = 500000.0
         BCr = BASE_MULTIPLIER.fdiv(500_000 * WAD, WAD);
         tokensOut = bondingSurface.tokenOut(10 * WAD, 1_000_000 * WAD, BCr);
-        assertApproxEqRel(tokensOut, 49.9995e18, REL_TOLERANCE); // 49.9995 * 1e18
+        assertApproxEqRel(tokensOut, 49.9995e18, REL_TOLERANCE);
 
         // Test case 4: Ca = 0.1, in = 0.1, Cr = 0.05
         BCr = BASE_MULTIPLIER.fdiv(5e16, WAD);
         tokensOut = bondingSurface.tokenOut(1e17, 1e17, BCr);
-        assertApproxEqRel(tokensOut, 2_500_000e18, REL_TOLERANCE); // 2500000.0 * 1e18
+        assertApproxEqRel(tokensOut, 2_500_000e18, REL_TOLERANCE);
     }
 
     function testTokenOut_revertsIfInvalidInputAmount() public {
@@ -103,36 +111,38 @@ contract BondingSurface_Test is Test {
     }
 
     function testTokenIn() public {
-        // Test case 1: Ca = 10000.0, Cr = 50.0
+        // Test case 1: Ca = 10000.0, Cr = 50.0, Tokens Burned = 1
         uint BCr = BASE_MULTIPLIER.fdiv(50 * WAD, WAD); // B/Cr
         uint capitalOut = bondingSurface.tokenIn(1 * WAD, 10_000 * WAD, BCr);
         console.log("Test case 1 - BCr:", BCr);
-        console.log("Expected:", uint(199_996_000_000_000_000)); // 0.199996 * 1e18
+        console.log("Expected:", uint(0.19999600008122798e18));
         console.log("Got:", capitalOut);
-        assertApproxEqRel(capitalOut, 199_996_000_000_000_000, REL_TOLERANCE); // 0.199996 * 1e18
+        assertApproxEqRel(capitalOut, 0.19999600008122798e18, REL_TOLERANCE);
 
-        // Test case 2: Ca = 50.0, Cr = 25.0
+        // Test case 2: Ca = 50.0, Cr = 25.0, Tokens Burned = 2.0
         BCr = BASE_MULTIPLIER.fdiv(25 * WAD, WAD);
-        capitalOut = bondingSurface.tokenIn(1 * WAD, 50 * WAD, BCr);
+        capitalOut = bondingSurface.tokenIn(2 * WAD, 50 * WAD, BCr);
         console.log("\nTest case 2 - BCr:", BCr);
-        console.log("Expected:", uint(19_999_900_000_000)); // 1.99999e-5 * 1e18
+        console.log("Expected:", uint(19_999_991_998_531)); // 1.9999991998531e-5 * 1e18
         console.log("Got:", capitalOut);
-        assertApproxEqRel(capitalOut, 19_999_900_000_000, REL_TOLERANCE); // 1.99999e-5 * 1e18
+        assertApproxEqAbs(capitalOut, 19_999_991_998_531, 10_000_000_000);
 
-        // Test case 3: Ca = 1000000.0, Cr = 500000.0
+        // Test case 3: Ca = 1000000.0, Cr = 500000.0, Tokens Burned = 100.0
         BCr = BASE_MULTIPLIER.fdiv(500_000 * WAD, WAD);
-        capitalOut = bondingSurface.tokenIn(1 * WAD, 1_000_000 * WAD, BCr);
+        capitalOut = bondingSurface.tokenIn(100 * WAD, 1_000_000 * WAD, BCr);
         console.log("\nTest case 3 - BCr:", BCr);
-        console.log("Expected:", uint(19_999_600_000_000_000_000)); // 19.9996 * 1e18
+        console.log("Expected:", uint(19.999600007897243e18));
         console.log("Got:", capitalOut);
-        assertApproxEqRel(capitalOut, 19_999_600_000_000_000_000, REL_TOLERANCE); // 19.9996 * 1e18
+        assertApproxEqRel(capitalOut, 19.999600007897243e18, REL_TOLERANCE);
 
-        // Test case 4: Ca = 0.1, Cr = 0.05
-        BCr = BASE_MULTIPLIER.fdiv(5e16, WAD);
-        capitalOut = bondingSurface.tokenIn(1 * WAD, 1e17, BCr);
+        // Test case 4: Ca = 0.1, Cr = 0.05, Tokens Burned = 0.5
+        BCr = BASE_MULTIPLIER.fdiv(50_000_000_000_000_000, WAD);
+        capitalOut = bondingSurface.tokenIn(
+            500_000_000_000_000_000, 100_000_000_000_000_000, BCr
+        );
         console.log("\nTest case 4 - BCr:", BCr);
-        console.log("Expected:", uint(999_999_000_000)); // 9.99999e-9 * 1e18
+        console.log("Expected:", uint(9_999_998_995)); // 9.999998995e-9 * 1e18
         console.log("Got:", capitalOut);
-        assertApproxEqRel(capitalOut, 999_999_000_000, REL_TOLERANCE); // 9.99999e-9 * 1e18
+        assertApproxEqRel(capitalOut, 9_999_998_995, REL_TOLERANCE);
     }
 }
