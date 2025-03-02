d0f052240e26d186eb19e65d6476f322376364a4
Fabian Scherer <fabiansche@gmail.com>
2025-03-02 21:52:24 +0400

    chore: fmt

--- src/templates/tests/unit/FM_Template_v1.t.sol
+++ src/templates/tests/unit/FM_Template_v1.t.sol
@@ -48,7 +48,7 @@ import {IFM_Template_v1} from "src/templates/modules/IFM_Template_v1.sol";
 contract FM_Template_v1_Test is ModuleTest {
     // =========================================================================
     // Constants
-    
+
     // =========================================================================
     // State
     FM_Template_v1_Exposed fundingManager;
