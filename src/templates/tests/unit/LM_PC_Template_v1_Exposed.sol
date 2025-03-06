1002df10fe36e0fbf72d4ef8fe44fe336146b750
Fabian Scherer <48454910+fabianschu@users.noreply.github.com>
2025-03-06 20:08:27 +0400

    fix: comment
    Co-authored-by: Marvin Kruse <marvinkruse@users.noreply.github.com>

--- src/templates/tests/unit/LM_PC_Template_v1_Exposed.sol
+++ src/templates/tests/unit/LM_PC_Template_v1_Exposed.sol
@@ -6,7 +6,7 @@ import {LM_PC_Template_v1} from "src/templates/modules/LM_PC_Template_v1.sol";
 
 // Access Mock of the PP_Template_v1 contract for Testing.
 contract LM_PC_Template_v1_Exposed is LM_PC_Template_v1 {
-    // Use the `exposed_` prefix for functions to expose internal contract for
+    // Use the `exposed_` prefix for functions to expose internal functions for
     // testing.
     function exposed_ensureValidDepositAmount(uint amount_) external pure {
         _ensureValidDepositAmount(amount_);
