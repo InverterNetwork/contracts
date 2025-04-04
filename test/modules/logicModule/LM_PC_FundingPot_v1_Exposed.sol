6f6f6f206dda63aef449a4b100c87173d6a8ab32
JeffreyJoel <jowoloko@gmail.com>
2025-04-04 12:17:48 +0100

    add exposed functions

--- test/modules/logicModule/LM_PC_FundingPot_v1_Exposed.sol
+++ test/modules/logicModule/LM_PC_FundingPot_v1_Exposed.sol
@@ -27,7 +27,7 @@ contract LM_PC_FundingPot_v1_Exposed is LM_PC_FundingPot_v1 {
     }
 
     /**
-     * @notice Exposes the internal _getUserPersonalCap function for testing
+     * @notice Exposes the internal _getUserPersonalCapForRound function for testing
      */
     function exposed_getUserPersonalCapForRound(
         uint64 roundId_,
@@ -38,12 +38,127 @@ contract LM_PC_FundingPot_v1_Exposed is LM_PC_FundingPot_v1 {
     }
 
     /**
-     * @notice Exposes the internal _getUnusedCapacityFromPreviousRounds function for testing
+     * @notice Exposes the internal _getUserUnusedCapacityFromPreviousRounds function for testing
      */
     function exposed_getUserUnusedCapacityFromPreviousRounds(
         address user_,
         uint64 currentRoundId_
     ) external view returns (uint) {
         return _getUserUnusedCapacityFromPreviousRounds(user_, currentRoundId_);
+    }
+
+    /**
+     * @notice Exposes the internal _validateRoundParameters function for testing
+     */
+    function exposed_validateRoundParameters(Round storage round_)
+        external
+        view
+    {
+        _validateRoundParameters(round_);
+    }
+
+    /**
+     * @notice Exposes the internal _validateEditRoundParameters function for testing
+     */
+    function exposed_validateEditRoundParameters(Round storage round_)
+        external
+        view
+    {
+        _validateEditRoundParameters(round_);
+    }
+
+    /**
+     * @notice Exposes the internal _validTimes function for testing
+     */
+    function exposed_validTimes(uint start_, uint cliff_, uint end_)
+        external
+        pure
+        returns (bool)
+    {
+        return _validTimes(start_, cliff_, end_);
+    }
+
+    /**
+     * @notice Exposes the internal _validateRoundContribution function for testing
+     */
+    function exposed_validateRoundContribution(
+        uint64 roundId_,
+        uint8 accessCriteriaId_,
+        bytes32[] calldata merkleProof_,
+        uint amount_
+    ) external view returns (uint) {
+        return _validateRoundContribution(
+            roundId_, accessCriteriaId_, merkleProof_, amount_
+        );
+    }
+
+    /**
+     * @notice Exposes the internal _validateAndAdjustCaps function for testing
+     */
+    function exposed_validateAndAdjustCaps(
+        uint64 roundId_,
+        uint amount_,
+        Round storage round_,
+        uint8 accessId_,
+        bool canOverrideContributionSpan_
+    ) external view returns (uint) {
+        return _validateAndAdjustCaps(
+            roundId_, amount_, round_, accessId_, canOverrideContributionSpan_
+        );
+    }
+
+    /**
+     * @notice Exposes the internal _validateAccessCriteria function for testing
+     */
+    function exposed_validateAccessCriteria(
+        uint64 roundId_,
+        uint8 accessId_,
+        bytes32[] calldata merkleProof_
+    ) external view {
+        _validateAccessCriteria(roundId_, accessId_, merkleProof_);
+    }
+
+    /**
+     * @notice Exposes the internal _checkAllowedAddressList function for testing
+     */
+    function exposed_checkAllowedAddressList(
+        address[] memory allowedAddresses_,
+        address sender_
+    ) external pure returns (bool) {
+        return _checkAllowedAddressList(allowedAddresses_, sender_);
+    }
+
+    /**
+     * @notice Exposes the internal _checkNftOwnership function for testing
+     */
+    function exposed_checkNftOwnership(address nftContract_, address user_)
+        external
+        view
+        returns (bool)
+    {
+        return _checkNftOwnership(nftContract_, user_);
+    }
+
+    /**
+     * @notice Exposes the internal _validateMerkleProof function for testing
+     */
+    function exposed_validateMerkleProof(
+        bytes32 root_,
+        bytes32[] memory merkleProof_,
+        address user_,
+        uint64 roundId_
+    ) external pure returns (bool) {
+        return _validateMerkleProof(root_, merkleProof_, user_, roundId_);
+    }
+
+    /**
+     * @notice Exposes the internal _recordContribution function for testing
+     */
+    function exposed_recordContribution(
+        uint64 roundId_,
+        address user_,
+        uint amount_
+    ) external {
+        _recordContribution(roundId_, user_, amount_);
     }
 }
