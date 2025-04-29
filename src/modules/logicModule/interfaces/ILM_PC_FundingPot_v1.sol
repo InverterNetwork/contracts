a62b027cc89b798e3e3c9e778e49741847e24c26
Fabian Scherer <fabiansche@gmail.com>
2025-04-29 22:25:07 +0200

    chore: formatting

--- src/modules/logicModule/interfaces/ILM_PC_FundingPot_v1.sol
+++ src/modules/logicModule/interfaces/ILM_PC_FundingPot_v1.sol
@@ -91,6 +91,7 @@ interface ILM_PC_FundingPot_v1 is IERC20PaymentClientBase_v2 {
         NFT, // 2
         MERKLE, // 3
         LIST // 4
+
     }
 
     // -------------------------------------------------------------------------
