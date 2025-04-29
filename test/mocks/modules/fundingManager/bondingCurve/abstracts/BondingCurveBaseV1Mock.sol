33Audits/inverter-contracts

Fix: Expose internal variable for testing in mock

test/modules/fundingManager/bondingCurve/utils/mocks/BondingCurveBaseV1Mock.sol
@@ -155,6 +155,10 @@ contract BondingCurveBaseV1Mock is BondingCurveBase_v1 {
         _setIssuanceToken(_newIssuanceToken);
     }
 
+    // Returns the internal variable of the issuance token storage
+    function call_issuanceToken() external view returns (address) {
+        return address(issuanceToken);
+    }
     // -------------------------------------------------------------------------
     // Helper function
 

test/modules/fundingManager/bondingCurve/abstracts/BondingCurveBase_v1.t.sol
@@ -926,11 +926,15 @@ contract BondingCurveBaseV1Test is ModuleTest {
             ERC20Issuance_v1(actualIssuanceToken).balanceOf(address(this)), 100
         );
 
-        // Create the wrapper and set it as the new issuance token
+        // Create the wrapper
         IssuanceTokenWrapperMock wrapper =
             new IssuanceTokenWrapperMock(actualIssuanceToken);
         assertEq(wrapper.issuanceToken(), actualIssuanceToken);
+
+        // Set the wrapper as the new issuance token
+        // and verify that it's set
         bondingCurveFundingManager.call_setIssuanceToken(address(wrapper));
+        assertEq(bondingCurveFundingManager.call_issuanceToken(), address(wrapper));
 
         // Obtain the issuance token again
         address issuanceTokenAfterWrapper =

InverterNetwork/contracts

test/mocks/modules/fundingManager/bondingCurve/abstracts/BondingCurveBaseV1Mock.sol
@@ -167,6 +167,10 @@ contract BondingCurveBaseV1Mock is BondingCurveBase_v1 {
         _projectFeeCollected(_workflowFeeAmount);
     }
 
+    // Returns the internal variable of the issuance token storage
+    function exposed_issuanceToken() external view returns (address) {
+        return address(issuanceToken);
+    }
     // -------------------------------------------------------------------------
     // Helper function
 

test/unit/modules/fundingManager/bondingCurve/abstracts/BondingCurveBase_v1.t.sol
@@ -890,11 +890,15 @@ contract BondingCurveBaseV1Test is ModuleTest {
             ERC20Issuance_v1(actualIssuanceToken).balanceOf(address(this)), 100
         );
 
-        // Create the wrapper and set it as the new issuance token
+        // Create the wrapper
         IssuanceTokenWrapperMock wrapper =
             new IssuanceTokenWrapperMock(actualIssuanceToken);
         assertEq(wrapper.issuanceToken(), actualIssuanceToken);
+
+        // Set the wrapper as the new issuance token
+        // and verify that it's set
         bondingCurveFundingManager.call_setIssuanceToken(address(wrapper));
+        assertEq(bondingCurveFundingManager.exposed_issuanceToken(), address(wrapper));
 
         // Obtain the issuance token again
         address issuanceTokenAfterWrapper =