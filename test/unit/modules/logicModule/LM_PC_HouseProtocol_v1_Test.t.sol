7ba7626db671160dfe833e52a0c9252630b0038c
Zuhaib Mohammed <zzzuhaibmohd@gmail.com>
2025-07-24 11:39:09 +0530

    test: configure the House Protocol with DBC FM

--- test/unit/modules/logicModule/LM_PC_HouseProtocol_v1_Test.t.sol
+++ test/unit/modules/logicModule/LM_PC_HouseProtocol_v1_Test.t.sol
@@ -9,6 +9,23 @@ import {
 } from "@unitTest/modules/ModuleTest.sol";
 import {OZErrors} from "@testUtilities/OZErrors.sol";
 import {ERC20Mock} from "@mocks/external/token/ERC20Mock.sol";
+import {IFundingManager_v1} from "@fm/IFundingManager_v1.sol";
+import {IBondingCurveBase_v1} from
+    "@fm/bondingCurve/interfaces/IBondingCurveBase_v1.sol";
+import {IRedeemingBondingCurveBase_v1} from
+    "@fm/bondingCurve/interfaces/IRedeemingBondingCurveBase_v1.sol";
+import {IVirtualCollateralSupplyBase_v1} from
+    "@fm/bondingCurve/interfaces/IVirtualCollateralSupplyBase_v1.sol";
+import {IVirtualIssuanceSupplyBase_v1} from
+    "@fm/bondingCurve/interfaces/IVirtualIssuanceSupplyBase_v1.sol";
+import {PackedSegment} from
+    "src/modules/fundingManager/bondingCurve/types/PackedSegment_v1.sol";
+import {IDiscreteCurveMathLib_v1} from
+    "src/modules/fundingManager/bondingCurve/interfaces/IDiscreteCurveMathLib_v1.sol";
+import {DiscreteCurveMathLib_v1} from
+    "src/modules/fundingManager/bondingCurve/formulas/DiscreteCurveMathLib_v1.sol";
+import {PackedSegmentLib} from
+    "src/modules/fundingManager/bondingCurve/libraries/PackedSegmentLib.sol";
 
 // External
 import {Clones} from "@oz/proxy/Clones.sol";
@@ -21,10 +38,15 @@ import {
     ERC20PaymentClientBaseV2Mock,
     ERC20Mock
 } from "@mocks/modules/paymentClient/ERC20PaymentClientBaseV2Mock.sol";
+import {ERC20Issuance_v1} from "@ex/token/ERC20Issuance_v1.sol"; // Added import
 
 // System under Test (SuT)
 import {ILM_PC_HouseProtocol_v1} from
     "@lm/interfaces/ILM_PC_HouseProtocol_v1.sol";
+import {IFM_BC_Discrete_Redeeming_VirtualSupply_v1} from
+    "src/modules/fundingManager/bondingCurve/interfaces/IFM_BC_Discrete_Redeeming_VirtualSupply_v1.sol";
+import {FM_BC_Discrete_Redeeming_VirtualSupply_v1_Exposed} from
+    "test/mocks/modules/fundingManager/bondingCurve/FM_BC_Discrete_Redeeming_VirtualSupply_v1_Exposed.sol";
 
 /**
  * @title   House Protocol Lending Facility Tests
@@ -40,6 +62,9 @@ import {ILM_PC_HouseProtocol_v1} from
  * @author  Inverter Network
  */
 contract LM_PC_HouseProtocol_v1_Test is ModuleTest {
+    using PackedSegmentLib for PackedSegment;
+    using DiscreteCurveMathLib_v1 for PackedSegment[];
+
     // =========================================================================
     // State
 
@@ -47,46 +72,200 @@ contract LM_PC_HouseProtocol_v1_Test is ModuleTest {
     LM_PC_HouseProtocol_v1_Exposed lendingFacility;
 
     // Mocks
-    ERC20Mock collateralToken;
-    ERC20Mock issuanceToken;
-    address dbcFmAddress;
+    // ERC20Mock collateralToken;
+    // ERC20Mock issuanceToken;
+    // address dbcFmAddress;
 
     // Test constants
     uint constant BORROWABLE_QUOTA = 8000; // 80% in basis points
     uint constant INDIVIDUAL_BORROW_LIMIT = 1000 ether;
     uint constant LOCKED_ISSUANCE_TOKENS = 1000 ether;
 
+    // Structs for organizing test data
+    struct CurveTestData {
+        PackedSegment[] packedSegmentsArray; // Array of PackedSegments for the library
+        uint totalCapacity; // Calculated: sum of segment capacities
+        uint totalReserve; // Calculated: sum of segment reserves
+        string description; // Optional: for logging or comments
+    }
+
+    // Protocol Fee Test Parameters
+    uint internal constant TEST_PROTOCOL_COLLATERAL_BUY_FEE_BPS = 50; // 0.5%
+    uint internal constant TEST_PROTOCOL_ISSUANCE_BUY_FEE_BPS = 20; // 0.2%
+    uint internal constant TEST_PROTOCOL_COLLATERAL_SELL_FEE_BPS = 40; // 0.4%
+    uint internal constant TEST_PROTOCOL_ISSUANCE_SELL_FEE_BPS = 30; // 0.3%
+    address internal constant TEST_PROTOCOL_TREASURY = address(0xFEE5); // Define a test treasury address
+
+    // Project Fee Constants (mirroring those in the contract for assertion)
+    uint internal constant TEST_PROJECT_BUY_FEE_BPS = 100;
+    uint internal constant TEST_PROJECT_SELL_FEE_BPS = 100;
+
+    address internal non_admin_address = address(0xB0B);
+
+    // Default Curve Parameters
+    uint public constant DEFAULT_SEG0_INITIAL_PRICE = 0.5 ether;
+    uint public constant DEFAULT_SEG0_PRICE_INCREASE = 0;
+    uint public constant DEFAULT_SEG0_SUPPLY_PER_STEP = 50 ether;
+    uint public constant DEFAULT_SEG0_NUMBER_OF_STEPS = 1;
+
+    uint public constant DEFAULT_SEG1_INITIAL_PRICE = 0.8 ether;
+    uint public constant DEFAULT_SEG1_PRICE_INCREASE = 0.02 ether;
+    uint public constant DEFAULT_SEG1_SUPPLY_PER_STEP = 25 ether;
+    uint public constant DEFAULT_SEG1_NUMBER_OF_STEPS = 2;
+
+    // Issuance Token Parameters
+    string internal constant ISSUANCE_TOKEN_NAME = "House Token";
+    string internal constant ISSUANCE_TOKEN_SYMBOL = "HOUSE";
+    uint8 internal constant ISSUANCE_TOKEN_DECIMALS = 18;
+    uint internal constant ISSUANCE_TOKEN_MAX_SUPPLY = type(uint).max;
+
+    FM_BC_Discrete_Redeeming_VirtualSupply_v1_Exposed public fmBcDiscrete;
+    ERC20Mock public orchestratorToken; // This is the collateral token
+    ERC20Issuance_v1 public issuanceToken; // This is the token to be issued
+    ERC20PaymentClientBaseV2Mock public paymentClient;
+    PackedSegment[] public initialTestSegments;
+    CurveTestData internal defaultCurve; // Declare defaultCurve variable
+
     // =========================================================================
     // Setup
 
     function setUp() public {
-        // Setup the tokens
-        collateralToken = new ERC20Mock("Collateral Token", "CT", 18);
-        issuanceToken = new ERC20Mock("Issuance Token", "IT", 18);
-        dbcFmAddress = makeAddr("dbcFm");
+        address impl_fmBcDiscrete =
+            address(new FM_BC_Discrete_Redeeming_VirtualSupply_v1_Exposed());
+        fmBcDiscrete = FM_BC_Discrete_Redeeming_VirtualSupply_v1_Exposed(
+            Clones.clone(impl_fmBcDiscrete)
+        );
+
+        orchestratorToken = new ERC20Mock("Orchestrator Token", "OTK", 18);
+        issuanceToken = new ERC20Issuance_v1(
+            ISSUANCE_TOKEN_NAME,
+            ISSUANCE_TOKEN_SYMBOL,
+            ISSUANCE_TOKEN_DECIMALS,
+            ISSUANCE_TOKEN_MAX_SUPPLY
+        );
+        // Grant minting rights for issuance token to the test contract for setup if needed,
+        // and later to the bonding curve itself.
+        issuanceToken.setMinter(address(this), true);
 
         // Deploy the SuT
-        address impl = address(new LM_PC_HouseProtocol_v1_Exposed());
-        lendingFacility = LM_PC_HouseProtocol_v1_Exposed(Clones.clone(impl));
+        address impl_lendingFacility =
+            address(new LM_PC_HouseProtocol_v1_Exposed());
+        lendingFacility =
+            LM_PC_HouseProtocol_v1_Exposed(Clones.clone(impl_lendingFacility));
 
         // Setup the module to test
+        _setUpOrchestrator(fmBcDiscrete); // This also sets up feeManager via _createFeeManager in ModuleTest
         _setUpOrchestrator(lendingFacility);
+
+        // Configure FeeManager *before* fmBcDiscrete.init() is called later in this setUp.
+        // ModuleTest's _setUpOrchestrator should make `this` (the test contract) the owner of feeManager.
+        feeManager.setWorkflowTreasury(
+            address(_orchestrator), TEST_PROTOCOL_TREASURY
+        );
+
+        bytes4 buyOrderSelector =
+            bytes4(keccak256(bytes("_buyOrder(address,uint,uint)")));
+        feeManager.setCollateralWorkflowFee(
+            address(_orchestrator),
+            address(fmBcDiscrete),
+            buyOrderSelector,
+            true,
+            TEST_PROTOCOL_COLLATERAL_BUY_FEE_BPS
+        );
+        feeManager.setIssuanceWorkflowFee(
+            address(_orchestrator),
+            address(fmBcDiscrete),
+            buyOrderSelector,
+            true,
+            TEST_PROTOCOL_ISSUANCE_BUY_FEE_BPS
+        );
+
+        bytes4 sellOrderSelector =
+            bytes4(keccak256(bytes("_sellOrder(address,uint,uint)")));
+        feeManager.setCollateralWorkflowFee(
+            address(_orchestrator),
+            address(fmBcDiscrete),
+            sellOrderSelector,
+            true,
+            TEST_PROTOCOL_COLLATERAL_SELL_FEE_BPS
+        );
+        feeManager.setIssuanceWorkflowFee(
+            address(_orchestrator),
+            address(fmBcDiscrete),
+            sellOrderSelector,
+            true,
+            TEST_PROTOCOL_ISSUANCE_SELL_FEE_BPS
+        );
+
+        _authorizer.setIsAuthorized(address(this), true);
+        _authorizer.grantRole(_authorizer.getAdminRole(), address(this));
+
+        defaultCurve.description = "Flat segment followed by a sloped segment";
+        uint[] memory initialPrices = new uint[](2);
+        initialPrices[0] = DEFAULT_SEG0_INITIAL_PRICE;
+        initialPrices[1] = DEFAULT_SEG1_INITIAL_PRICE;
+        uint[] memory priceIncreases = new uint[](2);
+        priceIncreases[0] = DEFAULT_SEG0_PRICE_INCREASE;
+        priceIncreases[1] = DEFAULT_SEG1_PRICE_INCREASE;
+        uint[] memory suppliesPerStep = new uint[](2);
+        suppliesPerStep[0] = DEFAULT_SEG0_SUPPLY_PER_STEP;
+        suppliesPerStep[1] = DEFAULT_SEG1_SUPPLY_PER_STEP;
+        uint[] memory numbersOfSteps = new uint[](2);
+        numbersOfSteps[0] = DEFAULT_SEG0_NUMBER_OF_STEPS;
+        numbersOfSteps[1] = DEFAULT_SEG1_NUMBER_OF_STEPS;
+
+        defaultCurve.packedSegmentsArray = helper_createSegments(
+            initialPrices, priceIncreases, suppliesPerStep, numbersOfSteps
+        );
+        defaultCurve.totalCapacity = (
+            DEFAULT_SEG0_SUPPLY_PER_STEP * DEFAULT_SEG0_NUMBER_OF_STEPS
+        ) + (DEFAULT_SEG1_SUPPLY_PER_STEP * DEFAULT_SEG1_NUMBER_OF_STEPS);
+        initialTestSegments = defaultCurve.packedSegmentsArray;
+
+        vm.expectEmit(true, true, true, true, address(fmBcDiscrete));
+        emit IBondingCurveBase_v1.IssuanceTokenSet(
+            address(issuanceToken), issuanceToken.decimals()
+        );
+        vm.expectEmit(true, true, true, true, address(fmBcDiscrete));
+        emit IFM_BC_Discrete_Redeeming_VirtualSupply_v1.SegmentsSet(
+            initialTestSegments
+        );
+        vm.expectEmit(true, true, true, true, address(fmBcDiscrete));
+        emit IFundingManager_v1.OrchestratorTokenSet(
+            address(orchestratorToken), orchestratorToken.decimals()
+        );
+
+        fmBcDiscrete.init(
+            _orchestrator,
+            _METADATA,
+            abi.encode(
+                address(issuanceToken),
+                address(orchestratorToken),
+                initialTestSegments
+            )
+        );
+
+        // Update protocol fee cache for buy and sell operations
+        fmBcDiscrete.updateProtocolFeeCache();
+
+        // Grant minting rights for issuance token to the bonding curve
+        issuanceToken.setMinter(address(fmBcDiscrete), true);
 
         // Initiate the Logic Module with the metadata and config data
         lendingFacility.init(
             _orchestrator,
             _METADATA,
             abi.encode(
-                address(collateralToken),
+                address(orchestratorToken),
                 address(issuanceToken),
-                dbcFmAddress,
+                address(fmBcDiscrete),
                 BORROWABLE_QUOTA,
                 INDIVIDUAL_BORROW_LIMIT
             )
         );
 
         // Mint tokens to the lending facility
-        collateralToken.mint(address(lendingFacility), 10_000 ether);
+        orchestratorToken.mint(address(lendingFacility), 10_000 ether);
         issuanceToken.mint(address(lendingFacility), 10_000 ether);
     }
 
@@ -97,6 +276,82 @@ contract LM_PC_HouseProtocol_v1_Test is ModuleTest {
     function testInit() public override(ModuleTest) {
         assertEq(
             address(lendingFacility.orchestrator()), address(_orchestrator)
+        );
+        assertEq(address(fmBcDiscrete.orchestrator()), address(_orchestrator));
+        assertEq(
+            address(fmBcDiscrete.token()),
+            address(orchestratorToken),
+            "Collateral token mismatch"
+        );
+        assertEq(
+            fmBcDiscrete.getIssuanceToken(),
+            address(issuanceToken),
+            "Issuance token mismatch"
+        );
+
+        PackedSegment[] memory segmentsAfterInit = fmBcDiscrete.getSegments();
+        assertEq(
+            segmentsAfterInit.length,
+            initialTestSegments.length,
+            "Segments length mismatch"
+        );
+        for (uint i = 0; i < segmentsAfterInit.length; i++) {
+            assertEq(
+                PackedSegment.unwrap(segmentsAfterInit[i]),
+                PackedSegment.unwrap(initialTestSegments[i]),
+                string(
+                    abi.encodePacked(
+                        "Segment content mismatch at index ", vm.toString(i)
+                    )
+                )
+            );
+        }
+
+        // --- Assertions for fee setup during init ---
+        assertEq(
+            fmBcDiscrete.buyFee(),
+            TEST_PROJECT_BUY_FEE_BPS,
+            "Project buy fee mismatch after init"
+        );
+        assertEq(
+            fmBcDiscrete.sellFee(),
+            TEST_PROJECT_SELL_FEE_BPS,
+            "Project sell fee mismatch after init"
+        );
+
+        IFM_BC_Discrete_Redeeming_VirtualSupply_v1.ProtocolFeeCache memory cache =
+            fmBcDiscrete.exposed_getProtocolFeeCache();
+
+        assertEq(
+            cache.collateralTreasury,
+            TEST_PROTOCOL_TREASURY,
+            "Cached collateral treasury mismatch"
+        );
+        assertEq(
+            cache.issuanceTreasury,
+            TEST_PROTOCOL_TREASURY,
+            "Cached issuance treasury mismatch"
+        ); // FeeManager uses one workflow treasury for both
+
+        assertEq(
+            cache.collateralFeeBuyBps,
+            TEST_PROTOCOL_COLLATERAL_BUY_FEE_BPS,
+            "Cached collateralFeeBuyBps mismatch"
+        );
+        assertEq(
+            cache.issuanceFeeBuyBps,
+            TEST_PROTOCOL_ISSUANCE_BUY_FEE_BPS,
+            "Cached issuanceFeeBuyBps mismatch"
+        );
+        assertEq(
+            cache.collateralFeeSellBps,
+            TEST_PROTOCOL_COLLATERAL_SELL_FEE_BPS,
+            "Cached collateralFeeSellBps mismatch"
+        );
+        assertEq(
+            cache.issuanceFeeSellBps,
+            TEST_PROTOCOL_ISSUANCE_SELL_FEE_BPS,
+            "Cached issuanceFeeSellBps mismatch"
         );
     }
 
@@ -296,9 +551,9 @@ contract LM_PC_HouseProtocol_v1_Test is ModuleTest {
         lendingFacility.borrow(borrowAmount);
 
         // Test: repay loan
-        collateralToken.mint(user, repayAmount);
+        orchestratorToken.mint(user, repayAmount);
         vm.prank(user);
-        collateralToken.approve(address(lendingFacility), repayAmount);
+        orchestratorToken.approve(address(lendingFacility), repayAmount);
 
         vm.prank(user);
         lendingFacility.repay(repayAmount);
@@ -329,9 +584,9 @@ contract LM_PC_HouseProtocol_v1_Test is ModuleTest {
         lendingFacility.borrow(borrowAmount);
 
         // Test: try to repay more than outstanding loan
-        collateralToken.mint(user, repayAmount);
+        orchestratorToken.mint(user, repayAmount);
         vm.prank(user);
-        collateralToken.approve(address(lendingFacility), repayAmount);
+        orchestratorToken.approve(address(lendingFacility), repayAmount);
 
         vm.prank(user);
         vm.expectRevert("Repayment amount exceeds outstanding loan");
@@ -532,5 +787,45 @@ contract LM_PC_HouseProtocol_v1_Test is ModuleTest {
         uint tokensToUnlock = lendingFacility
             .exposed_calculateIssuanceTokensToUnlock(user, repaymentAmount);
         assertEq(tokensToUnlock, 0); // No outstanding loan initially
+    }
+
+    // =========================================================================
+    // Helper Functions
+
+    function helper_createSegment(
+        uint _initialPrice,
+        uint _priceIncrease,
+        uint _supplyPerStep,
+        uint _numberOfSteps
+    ) internal pure returns (PackedSegment) {
+        return PackedSegmentLib._create(
+            _initialPrice, _priceIncrease, _supplyPerStep, _numberOfSteps
+        );
+    }
+
+    function helper_createSegments( // TODO: move to a library
+        uint[] memory _initialPrices,
+        uint[] memory _priceIncreases,
+        uint[] memory _suppliesPerStep,
+        uint[] memory _numbersOfSteps
+    ) internal pure returns (PackedSegment[] memory) {
+        require(
+            _initialPrices.length == _priceIncreases.length
+                && _initialPrices.length == _suppliesPerStep.length
+                && _initialPrices.length == _numbersOfSteps.length,
+            "Input arrays must have same length"
+        );
+
+        PackedSegment[] memory segments =
+            new PackedSegment[](_initialPrices.length);
+        for (uint i = 0; i < _initialPrices.length; i++) {
+            segments[i] = helper_createSegment(
+                _initialPrices[i],
+                _priceIncreases[i],
+                _suppliesPerStep[i],
+                _numbersOfSteps[i]
+            );
+        }
+        return segments;
     }
 }
